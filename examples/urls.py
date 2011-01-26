d38f7f3ef7ab803a8b9babda8f6be989f378429e
tom christie tom@tomchristie.com <none@none>
2011-01-26 21:09:08 +0000

    Fix broken merge

--- examples/urls.py
+++ examples/urls.py
@@ -4,15 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-<<<<<<< local
-    (r'^blog-post-api/', include('blogpost.urls')),
-    (r'^object-store-api/', include('objectstore.urls')),
-    (r'^pygments-api/', include('pygments_api.urls')),
-=======
-    (r'pygments-example/', include('pygmentsapi.urls')),
+    (r'pygments-example/', include('pygments_api.urls')),
     (r'^blog-post-example/', include('blogpost.urls')),
     (r'^object-store-example/', include('objectstore.urls')),
->>>>>>> other
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
