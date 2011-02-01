0c6f682cd0e03c06298cec78e79d10a87426fca8
tom christie tom@tomchristie.com <none@none>
2011-02-01 20:00:16 +0000

    Fix errant uppercasing in docs

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Django REST Framework"
+html_title = "Django REST framework"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
