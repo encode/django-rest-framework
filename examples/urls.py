c25f8f8427da13a969afab700aebb00d87d60838
tom christie tom@tomchristie.com <none@none>
2011-03-09 08:50:56 +0000

    fix #27

--- examples/urls.py
+++ examples/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 from sandbox.views import Sandbox
 
 urlpatterns = patterns('djangorestframework.views',
     (r'robots.txt', 'deny_robots'),
-    (r'favicon.ico', 'favicon'),
 
     (r'^$', Sandbox.as_view()),
 
@@ -17,3 +17,11 @@ urlpatterns = patterns('djangorestframework.views',
     (r'^accounts/login/$', 'api_login'),
     (r'^accounts/logout/$', 'api_logout'),
 )
+
+# Only serve favicon in production because otherwise chrome users will pretty much
+# permanantly have the django-rest-framework favicon whenever they navigate to
+# 127.0.0.1:8000 or whatever, which gets annoying
+if not settings.DEBUG:
+    urlpatterns += patterns('djangorestframework.views',
+        (r'favicon.ico', 'favicon'),
+    )
