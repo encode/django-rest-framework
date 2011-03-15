e3011c1fb8455fac58ca41acd38c32c0e791e85b
markotibold <none@none>
2011-03-15 22:57:45 +0100

    Implementing graphing.

--- djangorestframework/runtests/settings.py
+++ djangorestframework/runtests/settings.py
@@ -54,7 +54,7 @@ MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
+# Examples: "http://foo.com/media/", "/media/".    
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
@@ -95,3 +95,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'djangorestframework',
 )
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.run_tests'
+TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_DIR = 'xmlrunner'
