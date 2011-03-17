7856656df0c60d038f2d2c584e3bf9a5420cc801
tom christie tom@tomchristie.com <none@none>
2011-03-17 08:51:26 +0000

    Add VERSION string

---
+++ djangorestframework/tests/package.py
@@ -0,0 +1,11 @@
+"""Tests for the djangorestframework package setup."""
+from django.test import TestCase
+import djangorestframework
+
+class TestVersion(TestCase): 
+    """Simple sanity test to check the VERSION exists"""
+
+    def test_version(self):
+        """Ensure the VERSION exists."""
+        djangorestframework.VERSION
+
