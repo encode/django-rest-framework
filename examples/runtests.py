921519732728633517769eaa4a999f08100ce77d
markotibold <none@none>
2011-03-19 03:04:22 +0100

    Adding examples to tox.  Appears quite useful cause some example tests are broken.

---
+++ examples/runtests.py
@@ -0,0 +1,27 @@
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django.conf import settings
+from django.test.utils import get_runner
+
+def main():
+    TestRunner = get_runner(settings)
+
+    if hasattr(TestRunner, 'func_name'):
+        # Pre 1.2 test runners were just functions,
+        # and did not support the 'failfast' option.
+        import warnings
+        warnings.warn(
+            'Function-based test runners are deprecated. Test runners should be classes with a run_tests() method.',
+            DeprecationWarning
+        )
+        failures = TestRunner(None)
+    else:
+        test_runner = TestRunner()
+        failures = test_runner.run_tests(None)
+
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    main()
--- examples/settings.py
+++ examples/settings.py
@@ -105,3 +105,8 @@ INSTALLED_APPS = (
     'pygments_api',
     'blogpost',
 )
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_DIR = 'xmlrunner'
