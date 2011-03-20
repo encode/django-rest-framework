d168f42b134508cec44627ee7f423f1ab8c1d1a5
markotibold <none@none>
2011-03-20 01:34:24 +0100

    testing jenkins

--- examples/runtests.py
+++ examples/runtests.py
@@ -1,6 +1,6 @@
 import os
 import sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'examples.settings'
 
 from django.conf import settings
 from django.test.utils import get_runner
