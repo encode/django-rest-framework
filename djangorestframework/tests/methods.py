92b5a455da87c1d71d81903ea666daafb6b79725
Tom Christie <tomchristie@bright-interactive.co.uk>
2011-04-11 13:14:51 +0100

    Test cleanup

--- djangorestframework/tests/methods.py
+++ djangorestframework/tests/methods.py
@@ -1,33 +1,12 @@
-# TODO: Refactor these tests
 from django.test import TestCase
 from djangorestframework.compat import RequestFactory
 from djangorestframework.request import RequestMixin
-#from djangorestframework.methods import MethodMixin, StandardMethodMixin, OverloadedPOSTMethodMixin
-#
-#
+
+
 class TestMethodOverloading(TestCase): 
     def setUp(self):
         self.req = RequestFactory()
-#
-#    # Interface tests
-#
-#    def test_method_mixin_interface(self):
-#        """Ensure the base ContentMixin interface is as expected."""
-#        self.assertRaises(NotImplementedError, MethodMixin().determine_method, None)
-#
-#    def test_standard_method_mixin_interface(self):
-#        """Ensure the StandardMethodMixin interface is as expected."""
-#        self.assertTrue(issubclass(StandardMethodMixin, MethodMixin))
-#        getattr(StandardMethodMixin, 'determine_method')
-#
-#    def test_overloaded_method_mixin_interface(self):
-#        """Ensure the OverloadedPOSTMethodMixin interface is as expected."""
-#        self.assertTrue(issubclass(OverloadedPOSTMethodMixin, MethodMixin))
-#        getattr(OverloadedPOSTMethodMixin, 'METHOD_PARAM')
-#        getattr(OverloadedPOSTMethodMixin, 'determine_method')
-#
-#    # Behavioural tests
-#
+
     def test_standard_behaviour_determines_GET(self):
         """GET requests identified"""
         view = RequestMixin()
