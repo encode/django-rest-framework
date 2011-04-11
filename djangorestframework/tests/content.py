e29a3f4cf12ea4c7cc616f27441b44639c736334
Tom Christie <tomchristie@bright-interactive.co.uk>
2011-04-11 11:56:07 +0100

    More tests passing

--- djangorestframework/tests/content.py
+++ djangorestframework/tests/content.py
@@ -2,7 +2,7 @@
 from django.test import TestCase
 from djangorestframework.compat import RequestFactory
 from djangorestframework.request import RequestMixin
-from djangorestframework.parsers import FormParser, MultipartParser
+from djangorestframework.parsers import FormParser, MultipartParser, PlainTextParser
 #from djangorestframework.content import ContentMixin, StandardContentMixin, OverloadedContentMixin
 #
 #
@@ -43,28 +43,28 @@ class TestContentMixins(TestCase):
         view.request = self.req.post('/', data=form_data)
         self.assertEqual(view.RAW_CONTENT, form_data)
 
-    def ensure_determines_non_form_content_POST(self, mixin):
+    def ensure_determines_non_form_content_POST(self, view):
         """Ensure view.RAW_CONTENT returns content for POST request with non-form content."""
         content = 'qwerty'
         content_type = 'text/plain'
         view.parsers = (PlainTextParser,)
         view.request = self.req.post('/', content, content_type=content_type)
-        self.assertEqual(view.RAW_CONTENT, form_data)
+        self.assertEqual(view.RAW_CONTENT, content)
 
-    def ensure_determines_form_content_PUT(self, mixin):
+    def ensure_determines_form_content_PUT(self, view):
         """Ensure view.RAW_CONTENT returns content for PUT request with form content."""
         form_data = {'qwerty': 'uiop'}
         view.parsers = (FormParser, MultipartParser)
         view.request = self.req.put('/', data=form_data)
         self.assertEqual(view.RAW_CONTENT, form_data)
 
-    def ensure_determines_non_form_content_PUT(self, mixin):
+    def ensure_determines_non_form_content_PUT(self, view):
         """Ensure view.RAW_CONTENT returns content for PUT request with non-form content."""
         content = 'qwerty'
         content_type = 'text/plain'
         view.parsers = (PlainTextParser,)
         view.request = self.req.post('/', content, content_type=content_type)
-        self.assertEqual(view.RAW_CONTENT, form_data)#
+        self.assertEqual(view.RAW_CONTENT, content)
 
     def test_standard_behaviour_determines_no_content_GET(self):
         """Ensure request.RAW_CONTENT returns None for GET request with no content."""
