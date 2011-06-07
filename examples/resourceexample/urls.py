a8980892c47677f159499f7711cf812907dfaa47
Tom Christie <tomchristie@bright-interactive.co.uk>
2011-06-07 14:07:49 +0100

    Fix broken url naming in examples

--- examples/resourceexample/urls.py
+++ examples/resourceexample/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 from resourceexample.views import ExampleView, AnotherExampleView
 
 urlpatterns = patterns('',
-    url(r'^$',                 ExampleView.as_view(), name='example'),
+    url(r'^$',                 ExampleView.as_view(), name='example-resource'),
     url(r'^(?P<num>[0-9]+)/$', AnotherExampleView.as_view(), name='another-example'),
 )
