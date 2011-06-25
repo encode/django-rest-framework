fe7e3ba3ae692b5f20719d5de52682a57166ded2
David Larlet <larlet@gmail.com>
2011-06-25 18:42:03 +0200

    Adding tests for OAuth authentication through django-oauth-plus, a dedicated example project is still missing though

--- djangorestframework/runtests/settings.py
+++ djangorestframework/runtests/settings.py
@@ -94,6 +94,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'djangorestframework',
+    
+    'oauth_provider',
 )
 
 import os
