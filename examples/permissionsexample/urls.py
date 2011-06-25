bae21b14c93e458014107a007e894e93a181bd0e
markotibold <none@none>
2011-06-25 16:38:16 +0200

    added a container view for the permissions example

--- examples/permissionsexample/urls.py
+++ examples/permissionsexample/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from permissionsexample.views import ThrottlingExampleView
+from permissionsexample.views import PermissionsExampleView, ThrottlingExampleView, LoggedinView
 
 urlpatterns = patterns('',
-    url(r'^$', ThrottlingExampleView.as_view(), name='throttled-resource'),
+    url(r'^$', PermissionsExampleView.as_view(), name='permissions-example'),
+    url(r'^throttling$', ThrottlingExampleView.as_view(), name='throttled-resource'),
+    url(r'^loggedin$', LoggedinView.as_view(), name='loggedin-resource'),
 )
