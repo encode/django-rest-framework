e2b9359a2d903b2010dbf47565f63adebbdbc21b
Marko Tibold <marko@tibold.nl>
2011-12-15 00:27:29 +0100

    Adding a docs env to the tox project. This will run the linkcheck and build the html docs, turning any warnings into errors. Also changed the theme to sphinx-doc and added version to be displayed in the docs.

---
+++ docs/check_sphinx.py
@@ -0,0 +1,16 @@
+import pytest
+import subprocess
+
+def test_linkcheck(tmpdir):
+    doctrees = tmpdir.join("doctrees")
+    htmldir = tmpdir.join("html")
+    subprocess.check_call(
+        ["sphinx-build", "-W", "-blinkcheck",
+          "-d", str(doctrees), ".", str(htmldir)])
+
+def test_build_docs(tmpdir):
+    doctrees = tmpdir.join("doctrees")
+    htmldir = tmpdir.join("html")
+    subprocess.check_call([
+        "sphinx-build", "-W", "-bhtml",
+          "-d", str(doctrees), ".", str(htmldir)])
