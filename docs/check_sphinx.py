59904576a2dcdaae8a5537af0109ca23ae99b6bb
Marko Tibold <marko@tibold.nl>
2011-12-15 12:25:56 +0100

    reduced output to errors and warnings only

--- docs/check_sphinx.py
+++ docs/check_sphinx.py
@@ -5,12 +5,12 @@ def test_linkcheck(tmpdir):
     doctrees = tmpdir.join("doctrees")
     htmldir = tmpdir.join("html")
     subprocess.check_call(
-        ["sphinx-build", "-W", "-blinkcheck",
+        ["sphinx-build", "-q", "-blinkcheck",
           "-d", str(doctrees), ".", str(htmldir)])
 
 def test_build_docs(tmpdir):
     doctrees = tmpdir.join("doctrees")
     htmldir = "html" #we want to keep the docs
     subprocess.check_call([
-        "sphinx-build", "-W", "-bhtml",
+        "sphinx-build", "-q", "-bhtml",
           "-d", str(doctrees), ".", str(htmldir)])
