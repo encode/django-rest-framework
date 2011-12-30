c81c0d843ec555c4cc4b71f58852d2281a9cb5b2
Tom Christie <tom@tomchristie.com>
2011-12-30 16:30:55 +0000

    Fixes #102

--- djangorestframework/utils/staticviews.py
+++ djangorestframework/utils/staticviews.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import *
 from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import base64
 
 def deny_robots(request):
