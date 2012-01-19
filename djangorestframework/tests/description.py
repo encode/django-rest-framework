1db6b7cae4cfef96d67f8fb358ff59780e9b1b08
Ben Timby <btimby@gmail.com>
2012-01-19 15:07:24 -0500

    Implement docstring rendering for HTTP methods. Refactored get_name/get_description to use View methods for overriding.

--- djangorestframework/tests/description.py
+++ djangorestframework/tests/description.py
@@ -53,13 +53,13 @@ class TestViewNamesAndDescriptions(TestCase):
             pass
         self.assertEquals(get_name(MockView()), 'Mock')
 
-    # This has been turned off now.
-    #def test_resource_name_can_be_set_explicitly(self):
-    #    """Ensure Resource names can be set using the 'name' class attribute."""
-    #    example = 'Some Other Name'
-    #    class MockView(View):
-    #        name = example
-    #    self.assertEquals(get_name(MockView()), example)
+    def test_resource_name_can_be_set_explicitly(self):
+        """Ensure Resource names can be set using the 'get_name' method."""
+        example = 'Some Other Name'
+        class MockView(View):
+            def get_name(self):
+                return example
+        self.assertEquals(get_name(MockView()), example)
 
     def test_resource_description_uses_docstring_by_default(self):
         """Ensure Resource names are based on the docstring by default."""
@@ -81,21 +81,22 @@ class TestViewNamesAndDescriptions(TestCase):
 
         self.assertEquals(get_description(MockView()), DESCRIPTION)
 
-    # This has been turned off now
-    #def test_resource_description_can_be_set_explicitly(self):
-    #    """Ensure Resource descriptions can be set using the 'description' class attribute."""
-    #    example = 'Some other description'
-    #    class MockView(View):
-    #        """docstring"""
-    #        description = example
-    #    self.assertEquals(get_description(MockView()), example)
+    def test_resource_description_can_be_set_explicitly(self):
+        """Ensure Resource descriptions can be set using the 'get_description' method."""
+        example = 'Some other description'
+        class MockView(View):
+            """docstring"""
+            def get_description(self):
+                return example
+        self.assertEquals(get_description(MockView()), example)
 
-    #def test_resource_description_does_not_require_docstring(self):
-    #    """Ensure that empty docstrings do not affect the Resource's description if it has been set using the 'description' class attribute."""
-    #    example = 'Some other description'
-    #    class MockView(View):
-    #        description = example
-    #    self.assertEquals(get_description(MockView()), example)
+    def test_resource_description_does_not_require_docstring(self):
+        """Ensure that empty docstrings do not affect the Resource's description if it has been set using the 'get_description' method."""
+        example = 'Some other description'
+        class MockView(View):
+            def get_description(self):
+                return example
+        self.assertEquals(get_description(MockView()), example)
 
     def test_resource_description_can_be_empty(self):
         """Ensure that if a resource has no doctring or 'description' class attribute, then it's description is the empty string"""
