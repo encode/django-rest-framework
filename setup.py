c34e52f0b1d821b84a314e54781ad45322249c9d
Sean C. Farley <sean-dev@farley.org>
2012-02-01 14:16:34 -0500

    Exclude static/css from package_data since package_data does not handle the copying of subdirectories implicitly.

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ setup(
                 'djangorestframework.runtests',
                 'djangorestframework.utils'],
     package_dir={'djangorestframework': 'djangorestframework'},
-    package_data = {'djangorestframework': ['templates/*', 'static/*']},
+    package_data = {'djangorestframework': ['templates/*', 'static/*',
+        'static/css/*']},
+    exclude_package_data = {'djangorestframework': ['static/css']},
     test_suite = 'djangorestframework.runtests.runcoverage.main',
     install_requires=['URLObject>=0.6.0'],
     classifiers = [
