eea2aa04378d27d79e7aba12ce95c697148bd57e
Tom Christie <tom@tomchristie.com>
2012-08-29 19:54:38 +0100

    Remove examples (to be moved to a seperate project)

--- examples/pygments_api/models.py
+++
@@ -1,1 +0,0 @@
-#We need models.py otherwise the test framework complains (http://code.djangoproject.com/ticket/7198)
--- examples/requestexample/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-from requestexample.views import RequestExampleView, EchoRequestContentView
-from examples.views import ProxyView
-
-
-urlpatterns = patterns('',
-    url(r'^$', RequestExampleView.as_view(), name='request-example'),
-    url(r'^content$', ProxyView.as_view(view_class=EchoRequestContentView), name='request-content'),
-)
