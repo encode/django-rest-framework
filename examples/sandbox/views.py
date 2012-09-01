0f5dfb62b1f8648d222970fd2c39958fe3b16f16
Tom Christie <tom@tomchristie.com>
2012-09-01 23:04:14 +0100

    Fix broken link

--- examples/sandbox/views.py
+++ examples/sandbox/views.py
@@ -17,7 +17,7 @@ class Sandbox(View):
     The examples provided:
 
     1. A basic example using the [Resource](http://django-rest-framework.org/library/resource.html) class.
-    2. A basic example using the [ModelResource](http://django-rest-framework.org/library/modelresource.html) class.
+    2. A basic example using the [ModelResource](http://django-rest-framework.org/library/resource.html#resources.ModelResource) class.
     3. An basic example using Django 1.3's [class based views](http://docs.djangoproject.com/en/dev/topics/class-based-views/) and djangorestframework's [RendererMixin](http://django-rest-framework.org/library/renderers.html).
     4. A generic object store API.
     5. A code highlighting API.
