29dfbabaf54d75801b404e253a2d7574fa415564
Tom Christie <tom@tomchristie.com>
2012-09-04 09:32:02 +0100

    Remove support for markdown<2.1

--- djangorestframework/compat.py
+++ djangorestframework/compat.py
@@ -371,34 +371,6 @@ else:
 try:
     import markdown
 
-    class CustomSetextHeaderProcessor(markdown.blockprocessors.BlockProcessor):
-        """
-        Class for markdown < 2.1
-
-        Override `markdown`'s :class:`SetextHeaderProcessor`, so that ==== headers are <h2> and ---- heade
-
-        We use <h1> for the resource name.
-        """
-        import re
-        # Detect Setext-style header. Must be first 2 lines of block.
-        RE = re.compile(r'^.*?\n[=-]{3,}', re.MULTILINE)
-
-        def test(self, parent, block):
-            return bool(self.RE.match(block))
-
-        def run(self, parent, blocks):
-            lines = blocks.pop(0).split('\n')
-            # Determine level. ``=`` is 1 and ``-`` is 2.
-            if lines[1].startswith('='):
-                level = 2
-            else:
-                level = 3
-            h = markdown.etree.SubElement(parent, 'h%d' % level)
-            h.text = lines[0].strip()
-            if len(lines) > 2:
-                # Block contains additional lines. Add to  master blocks for later.
-                blocks.insert(0, '\n'.join(lines[2:]))
-
     def apply_markdown(text):
         """
         Simple wrapper around :func:`markdown.markdown` to set the base level
@@ -407,20 +379,12 @@ try:
 
         extensions = ['headerid(level=2)']
         safe_mode = False,
-
-        if markdown.version_info < (2, 1):
-            output_format = markdown.DEFAULT_OUTPUT_FORMAT
-
-            md = markdown.Markdown(extensions=markdown.load_extensions(extensions),
-                               safe_mode=safe_mode,
-                               output_format=output_format)
-            md.parser.blockprocessors['setextheader'] = CustomSetextHeaderProcessor(md.parser)
-        else:
-            md = markdown.Markdown(extensions=extensions, safe_mode=safe_mode)
+        md = markdown.Markdown(extensions=extensions, safe_mode=safe_mode)
         return md.convert(text)
 
 except ImportError:
     apply_markdown = None
+
 
 # Yaml is optional
 try:
