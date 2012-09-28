6cf2874cf2cdd60d559f472afcb1449e72eae2e1
Tom Christie <tom@tomchristie.com>
2012-09-28 16:09:52 +0100

    Clean up reverse tests

--- rest_framework/tests/reverse.py
+++ rest_framework/tests/reverse.py
@@ -1,26 +1,16 @@
 from django.conf.urls.defaults import patterns, url
 from django.test import TestCase
-from django.utils import simplejson as json
-
-from rest_framework.renderers import JSONRenderer
+from django.test.client import RequestFactory
 from rest_framework.reverse import reverse
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+factory = RequestFactory()
 
 
-class MyView(APIView):
-    """
-    Mock resource which simply returns a URL, so that we can ensure
-    that reversed URLs are fully qualified.
-    """
-    renderers = (JSONRenderer, )
-
-    def get(self, request):
-        return Response(reverse('myview', request=request))
-
+def null_view(request):
+    pass
 
 urlpatterns = patterns('',
-    url(r'^myview$', MyView.as_view(), name='myview'),
+    url(r'^view$', null_view, name='view'),
 )
 
 
@@ -31,5 +21,6 @@ class ReverseTests(TestCase):
     urls = 'rest_framework.tests.reverse'
 
     def test_reversed_urls_are_fully_qualified(self):
-        response = self.client.get('/myview')
-        self.assertEqual(json.loads(response.content), 'http://testserver/myview')
+        request = factory.get('/view')
+        url = reverse('view', request=request)
+        self.assertEqual(url, 'http://testserver/view')
