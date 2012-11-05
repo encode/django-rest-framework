bf8e67dfc27f85cccddcd1e9cdc68c46705503fc
Pavel Savchenko <pasha.savchenko@gmail.com>
2012-11-05 09:48:47 +0200

    Add the ability to add read_only_fields in bulk in serializer model

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -73,6 +73,7 @@ class SerializerOptions(object):
         self.depth = getattr(meta, 'depth', 0)
         self.fields = getattr(meta, 'fields', ())
         self.exclude = getattr(meta, 'exclude', ())
+        self.read_only_fields = getattr(meta, 'read_only_fields', ())
 
 
 class BaseSerializer(Field):
@@ -124,6 +125,9 @@ class BaseSerializer(Field):
         # Add in the default fields
         fields = self.default_fields(serialize, obj, data, nested)
         for key, val in fields.items():
+            if key in self.opts.read_only_fields:
+                val.read_only = True
+
             if key not in ret:
                 ret[key] = val
 
