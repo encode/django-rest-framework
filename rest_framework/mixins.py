3a30a9b1cbb4444adf3cbb1d3d80c637b5f4f2ca
Ludwig Kraatz <ludi182@hotmail.com>
2012-11-13 20:30:18 +0100

    removed useless line after polishing code
    added it in first commit but after third it became useless.

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -7,7 +7,6 @@ which allows mixin classes to be composed in interesting ways.
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.fields import HyperlinkedIdentityField
 
 
 class CreateModelMixin(object):
