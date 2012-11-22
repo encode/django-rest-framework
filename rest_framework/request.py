606c20f012c5a1fdcfd661eb280bab22b94afcf5
Xavier Ordoquy <xordoquy@linovia.com>
2012-11-22 02:08:00 +0100

    6 first tests passes under python 3.2

--- rest_framework/request.py
+++ rest_framework/request.py
@@ -20,7 +20,7 @@ def is_form_media_type(media_type):
     """
     Return True if the media type is a valid form media type.
     """
-    base_media_type, params = parse_header(media_type)
+    base_media_type, params = parse_header(media_type.encode('utf8'))
     return (base_media_type == 'application/x-www-form-urlencoded' or
             base_media_type == 'multipart/form-data')
 
