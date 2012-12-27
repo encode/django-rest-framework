24ed6dcfdadb5c1e7b18a1b1dfabad871ee91f09
Tom Christie <tom@tomchristie.com>
2012-12-27 17:35:00 +0000

    Update runtest settings to include staticfiles app.

--- rest_framework/runtests/settings.py
+++ rest_framework/runtests/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
