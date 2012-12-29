031a656667b1563516fa3cdb664da61243bbe00d
Tom Christie <tom@tomchristie.com>
2012-12-29 17:12:07 +0000

    Added hyperlinked FK create test

--- rest_framework/tests/relations_hyperlink.py
+++ rest_framework/tests/relations_hyperlink.py
@@ -234,6 +234,43 @@ class HyperlinkedForeignKeyTests(TestCase):
         ]
         self.assertEquals(serializer.data, expected)
 
+    def test_foreign_key_create(self):
+        data = {'url': '/foreignkeysource/4/', 'name': u'source-4', 'target': '/foreignkeytarget/2/'}
+        serializer = ForeignKeySourceSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        obj = serializer.save()
+        self.assertEquals(serializer.data, data)
+        self.assertEqual(obj.name, u'source-4')
+
+        # Ensure source 1 is updated, and everything else is as expected
+        queryset = ForeignKeySource.objects.all()
+        serializer = ForeignKeySourceSerializer(queryset)
+        expected = [
+            {'url': '/foreignkeysource/1/', 'name': u'source-1', 'target': '/foreignkeytarget/1/'},
+            {'url': '/foreignkeysource/2/', 'name': u'source-2', 'target': '/foreignkeytarget/1/'},
+            {'url': '/foreignkeysource/3/', 'name': u'source-3', 'target': '/foreignkeytarget/1/'},
+            {'url': '/foreignkeysource/4/', 'name': u'source-4', 'target': '/foreignkeytarget/2/'},
+        ]
+        self.assertEquals(serializer.data, expected)
+
+    # def test_reverse_foreign_key_create(self):
+    #     data = {'url': '/foreignkeytarget/3/', 'name': u'target-3', 'sources': ['/foreignkeysource/1/', '/foreignkeysource/3/']}
+    #     serializer = ForeignKeyTargetSerializer(data=data)
+    #     self.assertTrue(serializer.is_valid())
+    #     obj = serializer.save()
+    #     self.assertEquals(serializer.data, data)
+    #     self.assertEqual(obj.name, u'target-3')
+
+    #     # Ensure target 4 is added, and everything else is as expected
+    #     queryset = ForeignKeyTarget.objects.all()
+    #     serializer = ForeignKeyTargetSerializer(queryset)
+    #     expected = [
+    #         {'url': '/foreignkeytarget/1/', 'name': u'target-1', 'sources': ['/foreignkeysource/2/']},
+    #         {'url': '/foreignkeytarget/2/', 'name': u'target-2', 'sources': []},
+    #         {'url': '/foreignkeytarget/3/', 'name': u'target-3', 'sources': ['/foreignkeysource/1/', '/foreignkeysource/3/']},
+    #     ]
+    #     self.assertEquals(serializer.data, expected)
+
     def test_foreign_key_update_with_invalid_null(self):
         data = {'url': '/foreignkeysource/1/', 'name': u'source-1', 'target': None}
         instance = ForeignKeySource.objects.get(pk=1)
