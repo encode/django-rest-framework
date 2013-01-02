b807f3d52a68dbf657c6437f71ecbfcba0695972
Tom Christie <tom@tomchristie.com>
2013-01-02 13:39:24 +0000

    Keep API backwards compatible.

--- rest_framework/generics.py
+++ rest_framework/generics.py
@@ -47,14 +47,16 @@ class GenericAPIView(views.APIView):
 
         return serializer_class
 
-    def get_serializer(self, instance=None, data=None, files=None, partial=False):
+    def get_serializer(self, instance=None, data=None,
+                       files=None, partial=False):
         """
         Return the serializer instance that should be used for validating and
         deserializing input, and for serializing output.
         """
         serializer_class = self.get_serializer_class()
         context = self.get_serializer_context()
-        return serializer_class(instance, data=data, files=files, partial=partial, context=context)
+        return serializer_class(instance, data=data, files=files,
+                                partial=partial, context=context)
 
 
 class MultipleObjectAPIView(MultipleObjectMixin, GenericAPIView):
@@ -169,10 +171,11 @@ class UpdateAPIView(mixins.UpdateModelMixin,
     Concrete view for updating a model instance.
     """
     def put(self, request, *args, **kwargs):
-        return self.update(request, partial=False, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
-        return self.update(request, partial=True, *args, **kwargs)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
 
 
 class ListCreateAPIView(mixins.ListModelMixin,
@@ -212,10 +215,11 @@ class RetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.update(request, partial=False, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
-        return self.update(request, partial=True, *args, **kwargs)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
