a6454f7285ab05ae68a8680bd6c8ec8272e10a04
Mark Aaron Shirley <maspwr@gmail.com>
2013-01-02 21:06:32 +0100

    Add ForeignKeyTargetSource model to nested relation tests

--- rest_framework/tests/relations_nested.py
+++ rest_framework/tests/relations_nested.py
@@ -9,11 +9,19 @@ class ForeignKeyTarget(models.Model):
     name = models.CharField(max_length=100)
 
 
-class ForeignKeySource(models.Model):
+class ForeignKeyTargetSource(models.Model):
     name = models.CharField(max_length=100)
+    target = models.ForeignKey(ForeignKeyTarget, related_name='target_sources')
+
+
+class ForeignKeySource(models.Model):
+    name = models.CharField(max_length=100)    
     target = models.ForeignKey(ForeignKeyTarget, related_name='sources')
+    target_source = models.ForeignKey(ForeignKeyTargetSource, related_name='sources')
 
-
+    class Meta:
+        app_label = 'tests'
+    
 class ForeignKeySourceSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
@@ -25,12 +33,59 @@ class FlatForeignKeySourceSerializer(serializers.ModelSerializer):
         model = ForeignKeySource
 
 
+class FlatForeignKeyTargetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForeignKeyTarget
+
+
 class ForeignKeyTargetSerializer(serializers.ModelSerializer):
     sources = FlatForeignKeySourceSerializer()
 
     class Meta:
         model = ForeignKeyTarget
 
+
+class ForeignKeyTargetSourceSerializer1(serializers.ModelSerializer):
+    sources = FlatForeignKeySourceSerializer()
+
+    class Meta:
+        model = ForeignKeyTargetSource
+
+
+class ForeignKeyTargetSourceSerializer2(serializers.ModelSerializer):
+    target = FlatForeignKeyTargetSerializer()
+
+    class Meta:
+        model = ForeignKeyTargetSource
+
+
+class ReverseReverseSerializer(serializers.ModelSerializer):
+    target_sources = ForeignKeyTargetSourceSerializer1()
+
+    class Meta:
+        model = ForeignKeyTarget        
+
+
+class ReverseForwardSerializer(serializers.ModelSerializer):
+    sources = ForeignKeySourceSerializer()
+
+    class Meta:
+        model = ForeignKeyTargetSource
+
+
+class ForwardForwardSerializer(serializers.ModelSerializer):
+    target_sources = ForeignKeyTargetSourceSerializer2()
+
+    class Meta:
+        model = ForeignKeySource
+
+
+class ForwardReverseSerializer(serializers.ModelSerializer):
+    target = ForeignKeyTargetSerializer()
+
+    class Meta:
+        model = ForeignKeyTargetSource
+    
 
 # Nullable ForeignKey
 
@@ -46,15 +101,22 @@ class NullableForeignKeySourceSerializer(serializers.ModelSerializer):
         model = NullableForeignKeySource
 
 
-class ReverseForeignKeyTests(TestCase):
+class NestedForeignKeyTests(TestCase):
     def setUp(self):
         target = ForeignKeyTarget(name='target-1')
         target.save()
         new_target = ForeignKeyTarget(name='target-2')
         new_target.save()
+        target_source = ForeignKeyTargetSource(name='target-source-1', target=target)
+        target_source.save()
+        new_target_source = ForeignKeyTargetSource(name='target-source-2', target=target)
+        new_target_source.save()
         for idx in range(1, 4):
-            source = ForeignKeySource(name='source-%d' % idx, target=target)
+            import pdb
+            pdb.set_trace()
+            source = ForeignKeySource(name='source-%d' % idx, target=target, target_source=target_source)
             source.save()
+            
 
     def test_foreign_key_retrieve(self):
         queryset = ForeignKeySource.objects.all()
@@ -78,6 +140,25 @@ class ReverseForeignKeyTests(TestCase):
             {'id': 2, 'name': u'target-2', 'sources': [
             ]}
         ]
+        self.assertEquals(serializer.data, expected)
+
+    def test_reverse_foreign_key_create(self):
+        target = ForeignKeyTarget.objects.get(name='target-2')
+        # The value for target here should be ignored
+        data = {'sources': [{'name': u'source-4', 'target': 1},
+                            {'name': u'source-5', 'target': 1}],
+                'name': u'target-2'
+                }
+        expected = {'sources': [{'id': 4, 'name': u'source-4', 'target': 2},
+                                {'id': 5, 'name': u'source-5', 'target': 2}],
+                    'id': 2, 'name': u'target-2'
+                    }
+        serializer = ForeignKeyTargetSerializer(target, data=data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+        # Ensure target 2 has new source and everything else is as expected
+        target = ForeignKeyTarget.objects.get(name='target-2')
+        serializer = ForeignKeyTargetSerializer(target)
         self.assertEquals(serializer.data, expected)
 
 
