bbc11463203da3e76120596df661a8cc4ef3bf7e
Xavier Ordoquy <xordoquy@linovia.com>
2013-01-03 11:41:37 +0100

    Python 3 readiness

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
