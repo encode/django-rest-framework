fd061199140d976871b6d63c0d13d0c9862ae24b
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-01-04 21:30:37 +0000

    Pass through Serializer initialization kwargs.
    (from PaginationSerializer to the corresponding ObjectSerializer)

--- rest_framework/pagination.py
+++ rest_framework/pagination.py
@@ -62,7 +62,8 @@ class BasePaginationSerializer(serializers.Serializer):
         super(BasePaginationSerializer, self).__init__(*args, **kwargs)
         results_field = self.results_field
         object_serializer = self.opts.object_serializer_class
-        self.fields[results_field] = object_serializer(source='object_list')
+        kwargs['source'] = 'object_list'
+        self.fields[results_field] = object_serializer(*args, **kwargs)
 
     def to_native(self, obj):
         """
