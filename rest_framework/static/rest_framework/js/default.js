e53b66bed811e53a2430601ddb38eb78941dbdea
Michael Elovskikh <wronglink@gmail.com>
2013-01-25 15:18:22 +0600

    Added tabs between PUT and PATCH forms and better PATCH form handing

--- rest_framework/static/rest_framework/js/default.js
+++ rest_framework/static/rest_framework/js/default.js
@@ -3,3 +3,11 @@ prettyPrint();
 $('.js-tooltip').tooltip({
     delay: 1000
 });
+
+$('#patch-form').find('.field-switcher').on('change', function() {
+    var $this = $(this);
+    $('#patch-form').find('#'+$this.attr('data-field-id'))
+                    .prop('disabled', !$this.prop('checked'));
+});
+
+$('#form-method-switcher a:first').tab('show');
