cc13e95d5ef929644d7cccf55805313d14c3378b
Omer Katz <omer.drow@gmail.com>
2013-02-25 16:44:44 +0300

    Fixed misspelled remark in comments.

--- rest_framework/tests/generics.py
+++ rest_framework/tests/generics.py
@@ -344,7 +344,7 @@ class ExampleView(generics.ListCreateAPIView):
 class TestM2MBrowseableAPI(TestCase):
     def test_m2m_in_browseable_api(self):
         """
-        Test for particularly ugly reression with m2m in browseable API
+        Test for particularly ugly regression with m2m in browseable API
         """
         request = factory.get('/', HTTP_ACCEPT='text/html')
         view = ExampleView().as_view()
