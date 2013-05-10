fd4a66cfc7888775d20b18665d63156cf3dae13a
Tom Christie <tom@tomchristie.com>
2013-05-10 23:06:42 +0100

    Fix py3k compat with functools.reduce

--- rest_framework/filters.py
+++ rest_framework/filters.py
@@ -3,9 +3,9 @@ Provides generic filtering backends that can be used to filter the results
 returned by list views.
 """
 from __future__ import unicode_literals
-
 from django.db import models
 from rest_framework.compat import django_filters
+from functools import reduce
 import operator
 
 FilterSet = django_filters and django_filters.FilterSet or None
