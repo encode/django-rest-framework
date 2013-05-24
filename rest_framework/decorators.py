112b52f57e8f5a4a54e2b0c40f442ee63daf9709
Alex Burgel <aburgel@gmail.com>
2013-05-24 18:28:47 -0400

    Allow action decorator to handle multiple http methods

--- rest_framework/decorators.py
+++ rest_framework/decorators.py
@@ -112,18 +112,18 @@ def link(**kwargs):
     Used to mark a method on a ViewSet that should be routed for GET requests.
     """
     def decorator(func):
-        func.bind_to_method = 'get'
+        func.bind_to_methods = ['get']
         func.kwargs = kwargs
         return func
     return decorator
 
 
-def action(**kwargs):
+def action(methods=['post'], **kwargs):
     """
     Used to mark a method on a ViewSet that should be routed for POST requests.
     """
     def decorator(func):
-        func.bind_to_method = 'post'
+        func.bind_to_methods = methods
         func.kwargs = kwargs
         return func
     return decorator
