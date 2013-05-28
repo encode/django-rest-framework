a1601c1d6623533da11183126b9448a764382115
wndfly <wndfly@163.com>
2013-05-28 17:17:48 +0800

    Update routers.py
    modified generate route, now is looks more like restful route mode.

--- rest_framework/routers.py
+++ rest_framework/routers.py
@@ -72,7 +72,7 @@ class SimpleRouter(BaseRouter):
     routes = [
         # List route.
         Route(
-            url=r'^{prefix}/$',
+            url=r'^{prefix}$',
             mapping={
                 'get': 'list',
                 'post': 'create'
@@ -82,7 +82,7 @@ class SimpleRouter(BaseRouter):
         ),
         # Detail route.
         Route(
-            url=r'^{prefix}/{lookup}/$',
+            url=r'^{prefix}/{lookup}$',
             mapping={
                 'get': 'retrieve',
                 'put': 'update',
@@ -95,7 +95,7 @@ class SimpleRouter(BaseRouter):
         # Dynamically generated routes.
         # Generated using @action or @link decorators on methods of the viewset.
         Route(
-            url=r'^{prefix}/{lookup}/{methodname}/$',
+            url=r'^{prefix}/{lookup}/{methodname}$',
             mapping={
                 '{httpmethod}': '{methodname}',
             },
