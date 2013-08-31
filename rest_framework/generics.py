3063a50fc20f0bfb7308e668cf083c5ae0876dac
Edmond Wong <edmond@ugcs.caltech.edu>
2013-08-30 18:03:44 -0700

    Allow OPTIONS to retrieve PUT field metadata on empty objects
    This allows OPTIONS to return the PUT endpoint's object serializer metadata when the object hasn't been created yet.

--- rest_framework/generics.py
+++ rest_framework/generics.py
@@ -356,8 +356,13 @@ class GenericAPIView(views.APIView):
                 self.check_permissions(cloned_request)
                 # Test object permissions
                 if method == 'PUT':
-                    self.get_object()
-            except (exceptions.APIException, PermissionDenied, Http404):
+                    try:
+                        self.get_object()
+                    except Http404:
+                        # Http404 should be acceptable and the serializer
+                        # metadata should be populated.
+                        pass
+            except (exceptions.APIException, PermissionDenied):
                 pass
             else:
                 # If user has appropriate permissions for the view, include
