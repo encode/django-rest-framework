4807e00bffc0116630af42b86049f197f8d4fc99
George Hickman <george@ghickman.co.uk>
2013-11-15 15:49:53 +0000

    Set up wheel distribution support

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ version = get_version('rest_framework')
 
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
     print("You probably want to also tag the version now:")
     print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
