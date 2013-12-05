79596dc613bbf24aac7b5c56179cbc5c46eacdf3
Justin Davis <justin@chartio.com>
2013-12-05 13:17:23 -0800

    fix setup.py with new __init__.py boilerplate

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_version(package):
     Return package version as listed in `__version__` in `init.py`.
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 def get_packages(package):
