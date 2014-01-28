da89cfc9373c03115b1bc2f62a7f974363c6c050
Xavier Ordoquy <xordoquy@linovia.com>
2014-01-28 18:53:58 +0100

    Don’t rely on Django’s get as it breaks for 1.7

--- rest_framework/test.py
+++ rest_framework/test.py
@@ -71,6 +71,10 @@ class APIRequestFactory(DjangoRequestFactory):
 
         return ret, content_type
 
+    def get(self, path, data=None, format=None, content_type=None, **extra):
+        data, content_type = self._encode_data(data, format, content_type)
+        return self.generic('GET', path, data, content_type, **extra)
+
     def post(self, path, data=None, format=None, content_type=None, **extra):
         data, content_type = self._encode_data(data, format, content_type)
         return self.generic('POST', path, data, content_type, **extra)
