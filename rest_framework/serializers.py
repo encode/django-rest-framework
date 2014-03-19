c3aa10e589cb524dc3bb39a4fccee8238763d25a
Vladislav Vlastovskiy <me@vlastv.ru>
2014-03-20 01:50:40 +0400

    Moved get component from object after test source is star

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -438,25 +438,26 @@ class BaseSerializer(WritableField):
                     raise ValidationError(self.error_messages['required'])
                 return
 
-        # Set the serializer object if it exists
-        obj = get_component(self.parent.object, self.source or field_name) if self.parent.object else None
-
-        # If we have a model manager or similar object then we need
-        # to iterate through each instance.
-        if (self.many and
-            not hasattr(obj, '__iter__') and
-            is_simple_callable(getattr(obj, 'all', None))):
-            obj = obj.all()
-
-        if self.source == '*':
-            if value:
-                reverted_data = self.restore_fields(value, {})
-                if not self._errors:
-                    into.update(reverted_data)
         else:
             if value in (None, ''):
                 into[(self.source or field_name)] = None
             else:
+                # Set the serializer object if it exists
+                obj = get_component(self.parent.object, self.source or field_name) if self.parent.object else None
+
+                # If we have a model manager or similar object then we need
+                # to iterate through each instance.
+                if (self.many and
+                    not hasattr(obj, '__iter__') and
+                    is_simple_callable(getattr(obj, 'all', None))):
+                    obj = obj.all()
+
+                if self.source == '*':
+                    if value:
+                        reverted_data = self.restore_fields(value, {})
+                        if not self._errors:
+                            into.update(reverted_data)
+
                 kwargs = {
                     'instance': obj,
                     'data': value,
