566ad11b0aade421d9295ef92ae6e0f941d832e1
José Padilla <jpadilla@webapplicate.com>
2014-03-21 11:18:14 -0400

    Remove order_by from AutoFilterSet

--- rest_framework/filters.py
+++ rest_framework/filters.py
@@ -55,7 +55,6 @@ class DjangoFilterBackend(BaseFilterBackend):
                 class Meta:
                     model = queryset.model
                     fields = filter_fields
-                    order_by = True
             return AutoFilterSet
 
         return None
