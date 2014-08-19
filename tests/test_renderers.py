b1757516148b63d6a25b9c94c9f2231983d0aec2
Daniel Naab <danielnaab@gmail.com>
2014-08-19 18:33:39 -0500

    Add failing test for issue #1649:     https://github.com/tomchristie/django-rest-framework/issues/1649#issuecomment-52525940

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -101,6 +101,14 @@ class HTMLView1(APIView):
     def get(self, request, **kwargs):
         return Response('text')
 
+
+class HTMLView2(APIView):
+    renderer_classes = (BrowsableAPIRenderer, JSONRenderer)
+
+    def get(self, request, **kwargs):
+        return Response({'url': 'http://domain.com/?param=Yes+%26+No'})
+
+
 urlpatterns = patterns(
     '',
     url(r'^.*\.(?P<format>.+)$', MockView.as_view(renderer_classes=[RendererA, RendererB])),
@@ -111,6 +119,7 @@ urlpatterns = patterns(
     url(r'^parseerror$', MockPOSTView.as_view(renderer_classes=[JSONRenderer, BrowsableAPIRenderer])),
     url(r'^html$', HTMLView.as_view()),
     url(r'^html1$', HTMLView1.as_view()),
+    url(r'^html2$', HTMLView2.as_view()),
     url(r'^empty$', EmptyGETView.as_view()),
     url(r'^api', include('rest_framework.urls', namespace='rest_framework'))
 )
@@ -268,6 +277,16 @@ class RendererEndToEndTests(TestCase):
         self.assertContains(resp, '>GET, HEAD, OPTIONS<')
         self.assertContains(resp, '>application/json<')
         self.assertNotContains(resp, '>text/html; charset=utf-8<')
+
+    def test_browsable_api_urls(self):
+        """
+        Issue #1649
+
+        Test that URLs have properly escaped GET parameters.
+        """
+        resp = self.client.get('/html2')
+        # GET parameter should be escaped as Yes+%26+No, not Yes+&amp;+No
+        self.assertEqual(resp.rendered_content.find('Yes+&amp;+No'), -1)
 
 
 _flat_repr = '{"foo":["bar","baz"]}'
