1e491c82e6ddf2941d1f8639920c6b5dfd9378f8
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2014-09-02 15:29:21 +0200

    Fix imports

--- tests/browsable_api/auth_urls.py
+++ tests/browsable_api/auth_urls.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url, include
 
-from views import MockView
+from .views import MockView
 
 urlpatterns = patterns(
     '',
--- tests/browsable_api/no_auth_urls.py
+++ tests/browsable_api/no_auth_urls.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns
 
-from views import MockView
+from .views import MockView
 
 urlpatterns = patterns(
     '',
