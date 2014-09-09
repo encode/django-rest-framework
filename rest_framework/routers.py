02595eacf23a77efe6eac0ae384456d7bb0ea397
Gaurav Dadhania <gaurav@dadhania.in>
2014-09-09 05:35:44 +0530

    Fix APIRoot.get signature.

--- rest_framework/routers.py
+++ rest_framework/routers.py
@@ -284,10 +284,10 @@ class DefaultRouter(SimpleRouter):
         class APIRoot(views.APIView):
             _ignore_model_permissions = True
 
-            def get(self, request, format=None, *args, **kwargs):
+            def get(self, request, *args, **kwargs):
                 ret = {}
                 for key, url_name in api_root_dict.items():
-                    ret[key] = reverse(url_name, request=request, format=format)
+                    ret[key] = reverse(url_name, request=request, format=kwargs.get('format'))
                 return Response(ret)
 
         return APIRoot.as_view()
