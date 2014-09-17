a37db382c69293953c709365cd2c64c4a99f419e
José Padilla <jpadilla@webapplicate.com>
2014-09-17 09:01:49 -0400

    Update authtoken latest Django 1.7 migration

--- rest_framework/authtoken/migrations/0001_initial.py
+++ rest_framework/authtoken/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-# encoding: utf8
+
 from __future__ import unicode_literals
 
 from django.db import models, migrations
@@ -16,11 +16,10 @@ class Migration(migrations.Migration):
             name='Token',
             fields=[
                 ('key', models.CharField(max_length=40, serialize=False, primary_key=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, to_field='id')),
                 ('created', models.DateTimeField(auto_now_add=True)),
+                ('user', models.OneToOneField(related_name=b'auth_token', to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'abstract': False,
             },
             bases=(models.Model,),
         ),
