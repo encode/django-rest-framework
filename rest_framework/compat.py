3e69ed171dd5a623ffa7f45dc3fb3373199db47a
Omer Katz <omer.drow@gmail.com>
2014-10-02 17:18:32 +0300

    Added an optional LRU cache to the JSON encoder.

--- rest_framework/compat.py
+++ rest_framework/compat.py
@@ -310,3 +310,16 @@ except ImportError:
         klass.__unicode__ = klass.__str__
         klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
         return klass
+
+try:
+    from functools import lru_cache
+except ImportError:
+    try:
+        from backports.functools_lru_cache import lru_cache
+    except ImportError:
+        def lru_cache(maxsize=100, typed=False):  # When the LRU cache decorator is not available replace with a stub.
+            def decorating_function(f):
+                def wrapper(*args, **kwargs):
+                    return f(*args, **kwargs)
+                return wrapper
+            return decorating_function
