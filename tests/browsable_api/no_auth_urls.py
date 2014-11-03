6dbcbcc9a89f02bf050081076cab45b21ddffca0
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2014-11-03 16:25:25 -0500

    Failing test case for creation-only serializers.

--- tests/browsable_api/no_auth_urls.py
+++ tests/browsable_api/no_auth_urls.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns
 
-from .views import MockView
+from .views import MockView, CreateOnlyView
 
 urlpatterns = patterns(
     '',
     (r'^$', MockView.as_view()),
+    (r'^create/$', CreateOnlyView.as_view()),
 )
--- tests/browsable_api/views.py
+++ tests/browsable_api/views.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 from rest_framework.views import APIView
 from rest_framework import authentication
 from rest_framework import renderers
+from rest_framework import generics
+from rest_framework import serializers
 from rest_framework.response import Response
+
+from tests.models import BasicModel
 
 
 class MockView(APIView):
@@ -13,3 +17,19 @@ class MockView(APIView):
 
     def get(self, request):
         return Response({'a': 1, 'b': 2, 'c': 3})
+
+
+class CreateOnlySerializer(serializers.ModelSerializer):
+    def restore_object(self, attrs, instance=None):
+        assert instance is None, 'Cannot update models with this serializer'
+        return super(CreateOnlySerializer, self).restore_object(attrs, instance=instance)
+
+    class Meta:
+        model = BasicModel
+
+
+class CreateOnlyView(generics.ListCreateAPIView):
+    authentication_classes = (authentication.SessionAuthentication,)
+    renderer_classes = (renderers.BrowsableAPIRenderer,)
+    serializer_class = CreateOnlySerializer
+    model = BasicModel
