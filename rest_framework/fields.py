e49d22dbda5ac889ab89f277e17752c840819de2
Tom Christie <tom@tomchristie.com>
2014-11-19 09:31:26 +0000

    Allow blank choices to render. Closes #2071.

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -947,6 +947,8 @@ class ChoiceField(Field):
             self.fail('invalid_choice', input=data)
 
     def to_representation(self, value):
+        if value in ('', None):
+            return value
         return self.choice_strings_to_values[six.text_type(value)]
 
 
