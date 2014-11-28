d9a09f7826d7faf7e6bfa10fc83e0b7db4689a72
Tymur Maryokhin <maryokhin@gmail.com>
2014-11-28 13:33:07 +0100

    Removed unused import

--- rest_framework/authtoken/views.py
+++ rest_framework/authtoken/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework import parsers
 from rest_framework import renderers
 from rest_framework.response import Response
