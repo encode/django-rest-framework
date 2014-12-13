0e42c2e6637d60b864347821a84f48ebee79fd8c
Ben Oswald <ben.oswald@root-space.de>
2014-12-13 18:09:21 +0100

    add read_only check for dotted-source fields in serializers
    The error message tells that you should create an own crate() function
    or set read_only=True for the field. The Problem is that the check
    ignores the read_only flag.

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -629,7 +629,7 @@ def raise_errors_on_nested_writes(method_name, serializer, validated_data):
     #     ...
     #     address = serializer.CharField('profile.address')
     assert not any(
-        '.' in field.source and (key in validated_data)
+        '.' in field.source and (key in validated_data) and (field.read_only is not True)
         for key, field in serializer.fields.items()
     ), (
         'The `.{method_name}()` method does not support writable dotted-source '
