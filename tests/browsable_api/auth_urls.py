b09ef28959fe63351f0dd24564b7d2d344b44fa3
Brandon Cazander <brandon@bcdynamic.com>
2015-01-24 01:37:23 -0800

    Add failing test for request.version AttributeError in BrowsableAPI.

--- tests/browsable_api/auth_urls.py
+++ tests/browsable_api/auth_urls.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url, include
+from rest_framework import routers
 
-from .views import MockView
+from .views import MockView, FooViewSet, BarViewSet
+
+router = routers.SimpleRouter()
+router.register(r'foo', FooViewSet)
+router.register(r'bar', BarViewSet)
 
 urlpatterns = patterns(
     '',
     (r'^$', MockView.as_view()),
+    url(r'^', include(router.urls)),
+    url(r'^bar/(?P<pk>\d+)/$', BarViewSet, name='bar-list'),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
