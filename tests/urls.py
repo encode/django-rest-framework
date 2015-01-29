77d061d234e03004f34058028707ecddfc730fae
Brandon Cazander <brandon.cazander@multapplied.net>
2015-01-28 17:08:34 -0800

    Provide rest_framework.resolve. Fixes #2489

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,6 @@
 """
 Blank URLConf just to keep the test suite happy
 """
-from django.conf.urls import patterns
+from tests import test_relations
 
-urlpatterns = patterns('')
+urlpatterns = test_relations.urlpatterns
