c3425accdedb64e99238619f8f740bb547b2ecef
Tom Christie <tom@tomchristie.com>
2015-02-11 14:19:07 +0000

    Fix incorrect HTML parsing for DictField

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -1192,9 +1192,9 @@ class DictField(Field):
 
     def get_value(self, dictionary):
         # We override the default field access in order to support
-        # lists in HTML forms.
+        # dictionaries in HTML forms.
         if html.is_html_input(dictionary):
-            return html.parse_html_list(dictionary, prefix=self.field_name)
+            return html.parse_html_dict(dictionary, prefix=self.field_name)
         return dictionary.get(self.field_name, empty)
 
     def to_internal_value(self, data):
