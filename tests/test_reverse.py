fac27853418699116304ad8d77270fe9a20873dc
Ion Scerbatiuc <ion@rover.com>
2015-03-19 16:12:28 -0700

    Handling the fallback in `reverse`

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import NoReverseMatch
 from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APIRequestFactory
@@ -16,6 +17,18 @@ urlpatterns = patterns(
 )
 
 
+class MockVersioningScheme(object):
+
+    def __init__(self, raise_error=False):
+        self.raise_error = raise_error
+
+    def reverse(self, *args, **kwargs):
+        if self.raise_error:
+            raise NoReverseMatch()
+
+        return 'http://scheme-reversed/view'
+
+
 class ReverseTests(TestCase):
     """
     Tests for fully qualified URLs when using `reverse`.
@@ -24,5 +37,19 @@ class ReverseTests(TestCase):
 
     def test_reversed_urls_are_fully_qualified(self):
         request = factory.get('/view')
+        url = reverse('view', request=request)
+        self.assertEqual(url, 'http://testserver/view')
+
+    def test_reverse_with_versioning_scheme(self):
+        request = factory.get('/view')
+        request.versioning_scheme = MockVersioningScheme()
+
+        url = reverse('view', request=request)
+        self.assertEqual(url, 'http://scheme-reversed/view')
+
+    def test_reverse_with_versioning_scheme_fallback_to_default_on_error(self):
+        request = factory.get('/view')
+        request.versioning_scheme = MockVersioningScheme(raise_error=True)
+
         url = reverse('view', request=request)
         self.assertEqual(url, 'http://testserver/view')
