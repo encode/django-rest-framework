8ace80c287214a37e67b1c6d6926ae35d0e64a61
Dougal Matthews <dougal@redhat.com>
2015-04-23 22:18:35 +0100

    Only load the search results when the search form is submitted for the first time

--- docs_theme/js/theme.js
+++ docs_theme/js/theme.js
@@ -1,5 +1,35 @@
-$(function(){
+function getSearchTerm()
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == 'q')
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+$(function() {
+
+    var initialise_search = function(){
+        require.config({"baseUrl":"/mkdocs/js"});
+        require(["search",]);
+    }
+
+    var search_term = getSearchTerm();
+    if(search_term){
+        $('#mkdocs_search_modal').modal();
+    }
 
     $('pre code').parent().addClass('prettyprint well');
+
+    $(document).on("submit", "#mkdocs_search_modal form", function (e) {
+        $("#mkdocs-search-results").html("Searching...")
+        initialise_search();
+        return false;
+    });
 
 });
