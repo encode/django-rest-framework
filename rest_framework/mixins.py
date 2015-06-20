b933d595d22f6695fb051c4345a5f2b24e3b240a
jeffrey k eliasen <jeff@jke.net>
2015-06-19 14:43:34 -1000

    allow user to pass additional fields to constructor

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -17,12 +17,12 @@ class CreateModelMixin(object):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        self.perform_create(serializer, **(kwargs.get('extra_fields', {})))
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def perform_create(self, serializer, **kwargs):
+        serializer.save(**kwargs)
 
     def get_success_headers(self, data):
         try:
