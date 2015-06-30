f0bced64134b10c2cafbb206c04fe0fa774d205d
Laszlo Marai <atleta@atleta.hu>
2015-06-30 05:49:44 +0200

    Handle AttributeError as well (when the imported module exists but the specified function/class does not).

--- rest_framework/settings.py
+++ rest_framework/settings.py
@@ -171,7 +171,7 @@ def import_from_string(val, setting_name):
         module_path, class_name = '.'.join(parts[:-1]), parts[-1]
         module = importlib.import_module(module_path)
         return getattr(module, class_name)
-    except ImportError as e:
+    except (ImportError, AttributeError) as e:
         msg = "Could not import '%s' for API setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
         raise ImportError(msg)
 
