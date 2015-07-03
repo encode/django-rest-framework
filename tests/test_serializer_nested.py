b0c5f9db2f28324d77a841f504a24a5b51935552
Juan Gutierrez <juanny.gee@gmail.com>
2015-07-03 12:28:48 -0400

    allow a non-required nested serializer to validate correctly when receiving multipart data

--- tests/test_serializer_nested.py
+++ tests/test_serializer_nested.py
@@ -38,3 +38,46 @@ class TestNestedSerializer:
         }
         serializer = self.Serializer()
         assert serializer.data == expected_data
+
+
+class TestNotRequiredNestedSerializer:
+    def setup(self):
+        class NestedSerializer(serializers.Serializer):
+            one = serializers.IntegerField(max_value=10)
+
+        class TestSerializer(serializers.Serializer):
+            nested = NestedSerializer(required=False)
+
+        self.Serializer = TestSerializer
+
+        class FakeMultiDict(dict):
+            """
+            Use this to fake a `format="multipart"` request, because
+            `utils.is_html_input()` returns `True` when the dict object has
+            an attribute of "getlist".
+            """
+            def getlist(self, value, default=None):
+                if value in self:
+                    return [self[value]]
+                else:
+                    return [] if default is None else default
+
+        self.FakeMultiDict = FakeMultiDict
+
+    def test_json_validate(self):
+        input_data = {}
+        serializer = self.Serializer(data=input_data)
+        assert serializer.is_valid()
+
+        input_data = {'nested': {'one': '1'}}
+        serializer = self.Serializer(data=input_data)
+        assert serializer.is_valid()
+
+    def test_multipart_validate(self):
+        input_data = self.FakeMultiDict()
+        serializer = self.Serializer(data=input_data)
+        assert serializer.is_valid()
+
+        input_data = self.FakeMultiDict(**{'nested.one': '1'})
+        serializer = self.Serializer(data=input_data)
+        assert serializer.is_valid()
