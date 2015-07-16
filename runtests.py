db40b7eb916bbff2d0a4e4a8b5340a3ef866611f
Tom Christie <tom@tomchristie.com>
2015-07-16 12:39:01 +0100

    Add coverage to runtests. Closes #2936.

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ import sys
 import pytest
 
 PYTEST_ARGS = {
-    'default': ['tests', '--tb=short', '-s'],
+    'default': ['tests', '--tb=short', '-s', '--cov', 'rest_framework'],
     'fast': ['tests', '--tb=short', '-q', '-s'],
 }
 
