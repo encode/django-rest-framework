d2ae7d4fcd96e26fc102ad581859cc72a9a7551c
Hachikooking <none@none.com>
2015-07-29 14:43:25 +0200

    Add args to the function call for unnamed parameters

--- rest_framework/routers.py
+++ rest_framework/routers.py
@@ -291,6 +291,7 @@ class DefaultRouter(SimpleRouter):
                     try:
                         ret[key] = reverse(
                             url_name,
+                            args=args,
                             kwargs=kwargs,
                             request=request,
                             format=kwargs.get('format', None)
