6e86a53c922f09785869e022a7ccf51928a55382
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2015-09-22 16:14:45 +0200

    Remove `apps.get_model` fallback
    * Corrects presumed logic error in `ResolveModelWithPatchedDjangoTests`

--- rest_framework/utils/model_meta.py
+++ rest_framework/utils/model_meta.py
@@ -8,11 +8,12 @@ Usage: `get_field_info(model)` returns a `FieldInfo` instance.
 import inspect
 from collections import namedtuple
 
+from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.utils import six
 
-from rest_framework.compat import OrderedDict, get_model
+from rest_framework.compat import OrderedDict
 
 FieldInfo = namedtuple('FieldResult', [
     'pk',  # Model field instance
@@ -45,7 +46,7 @@ def _resolve_model(obj):
     """
     if isinstance(obj, six.string_types) and len(obj.split('.')) == 2:
         app_name, model_name = obj.split('.')
-        resolved_model = get_model(app_name, model_name)
+        resolved_model = apps.get_model(app_name, model_name)
         if resolved_model is None:
             msg = "Django did not return a model for {0}.{1}"
             raise ImproperlyConfigured(msg.format(app_name, model_name))
