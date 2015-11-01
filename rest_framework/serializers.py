646cadcc23b7f7500a99dbf95189e2de516f517c
Matías Lang <shareman1204@gmail.com>
2015-11-01 00:42:13 -0300

    Fix incorrect naming of field_kwargs in last commit

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -1160,7 +1160,7 @@ class ModelSerializer(Serializer):
         Create a read only field for model methods and properties.
         """
         field_class = ReadOnlyField
-        kwargs = {'help_text': getattr(model, field_name).__doc__}
+        field_kwargs = {'help_text': getattr(model, field_name).__doc__}
 
         return field_class, field_kwargs
 
