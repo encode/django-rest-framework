fc0ec571c26c03cafc9066ae25c13b54dd18ab65
Petros Moisiadis <ernest0x@yahoo.gr>
2015-12-16 20:59:46 +0200

    Include 'initial' attribute of serializer fields in metadata responses
    Frontend applications may now use the response of an OPTIONS request so as to populate the fields for "add/new/create" type of forms with initial values based on the 'initial' attribute of the serializer's fields.

--- rest_framework/metadata.py
+++ rest_framework/metadata.py
@@ -124,7 +124,8 @@ class SimpleMetadata(BaseMetadata):
         attrs = [
             'read_only', 'label', 'help_text',
             'min_length', 'max_length',
-            'min_value', 'max_value'
+            'min_value', 'max_value',
+            'initial'
         ]
 
         for attr in attrs:
