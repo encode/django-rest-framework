5b348721c2dc60b2d21bbb38c17c8358a47fc4cf
CaffeineFueled <CaffeineFueled@nullvalue.com>
2016-04-02 19:45:30 -0400

    fixed isort spacing issue

--- tests/test_request.py
+++ tests/test_request.py
@@ -19,7 +19,6 @@ from rest_framework.response import Response
 from rest_framework.test import APIClient, APIRequestFactory
 from rest_framework.views import APIView
 
-
 django_factory = DjangoRequestFactory()
 factory = APIRequestFactory()
 
