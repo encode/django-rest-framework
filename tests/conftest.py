0ab1aeb9ab1c4136155f6bf184a6bae259ebf3a8
Tom Christie <tom@tomchristie.com>
2016-06-01 14:01:00 +0100

    Remove code style issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def pytest_configure():
         USE_L10N=True,
         STATIC_URL='/static/',
         ROOT_URLCONF='tests.urls',
-        TEMPLATES = [
+        TEMPLATES=[
             {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
                 'APP_DIRS': True,
