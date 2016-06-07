2c0bcc6275b76c89be8fa674df199b4f73725cf1
Asif Saifuddin Auvi <auvipy@gmail.com>
2016-06-07 16:40:02 +0600

    reverted settings removal

--- rest_framework/authtoken/models.py
+++ rest_framework/authtoken/models.py
@@ -1,6 +1,7 @@
 import binascii
 import os
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
