df300d36c49ba8b4264c866506ee57324282831c
Mojca Rojko <mojca.rojko@gmail.com>
2016-06-13 14:21:21 +0200

    organise imports

--- rest_framework/authtoken/serializers.py
+++ rest_framework/authtoken/serializers.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 from rest_framework.fields import empty
-from django.contrib.auth import get_user_model
 
 
 class AuthTokenSerializer(serializers.Serializer):
