46a7062c9ade00c622de26739787181cdfc2f000
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-20 22:59:39 -0300

    Added reference to RFC 4918

--- rest_framework/status.py
+++ rest_framework/status.py
@@ -3,6 +3,7 @@ Descriptive HTTP status codes, for code readability.
 
 See RFC 2616 - http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 And RFC 6585 - http://tools.ietf.org/html/rfc6585
+And RFC 4918 - https://tools.ietf.org/html/rfc4918
 """
 from __future__ import unicode_literals
 
