4a32f6857fa1ae86ee94d6829eefbdc249c52b1c
Benjamin Dummer <dummerbenjamin@gmail.com>
2016-08-09 03:55:32 -0400

    Issue with newer Django versions

--- rest_framework/apps.py
+++ rest_framework/apps.py
@@ -2,8 +2,6 @@ from django.apps import AppConfig
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-from rest_framework.compat import importlib
-
 
 class RestFrameworkConfig(AppConfig):
     name = 'rest_framework'
@@ -15,6 +13,7 @@ class RestFrameworkConfig(AppConfig):
 
         This lets us evaluate all of the @router.route('...') calls.
         """
+        from rest_framework.compat import importlib
         for app in settings.INSTALLED_APPS:
             for mod_name in ['api']:
                 try:
