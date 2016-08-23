ec4761225e5a1683b046752e7090e91abfa52f54
dfavato <dfavato@gmail.com>
2016-08-23 14:52:20 -0300

    Set self.count before self.limit in LimitOffsetPagination
    By doing this it is possible to override get_limit in order to return all records if the request has a predefined param.
    
    For example, if one wants that all records are retrieved if url has &limit=-1, get_limit could return self.count in this case.
    
    Otherwise, if self.count is set after self.limit then, to achive the same result, one has to override get_limit and paginate_queryset, or run get_limit twice.

--- rest_framework/pagination.py
+++ rest_framework/pagination.py
@@ -303,12 +303,12 @@ class LimitOffsetPagination(BasePagination):
     template = 'rest_framework/pagination/numbers.html'
 
     def paginate_queryset(self, queryset, request, view=None):
+        self.count = _get_count(queryset)
         self.limit = self.get_limit(request)
         if self.limit is None:
             return None
 
         self.offset = self.get_offset(request)
-        self.count = _get_count(queryset)
         self.request = request
         if self.count > self.limit and self.template is not None:
             self.display_page_controls = True
