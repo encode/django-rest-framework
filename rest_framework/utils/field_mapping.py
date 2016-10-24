2a9483d8d50044593f89bd5ab766ebca05e252bd
Adrien <abrunet@centrale-marseille.fr>
2016-10-24 15:50:10 +0200

    revert stupid commit which wasn't a fix at all...

--- rest_framework/utils/field_mapping.py
+++ rest_framework/utils/field_mapping.py
@@ -12,13 +12,7 @@ from rest_framework.compat import DecimalValidator
 from rest_framework.validators import UniqueValidator
 
 NUMERIC_FIELD_TYPES = (
-    models.BigIntegerField,
-    models.DecimalField,
-    models.FloatField,
-    models.IntegerField,
-    models.PositiveIntegerField,
-    models.PositiveSmallIntegerField,
-    models.SmallIntegerField,
+    models.IntegerField, models.FloatField, models.DecimalField
 )
 
 
