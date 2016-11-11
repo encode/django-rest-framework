24791cb353d1924086b30abe2188280547d9a6c4
Tom Christie <tom@tomchristie.com>
2016-11-11 09:44:35 +0000

    Invalidate any existing prefetch cache on PUT requests. (#4668)

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -71,9 +71,8 @@ class UpdateModelMixin(object):
 
         if getattr(instance, '_prefetched_objects_cache', None):
             # If 'prefetch_related' has been applied to a queryset, we need to
-            # refresh the instance from the database.
-            instance = self.get_object()
-            serializer = self.get_serializer(instance)
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
 
         return Response(serializer.data)
 
