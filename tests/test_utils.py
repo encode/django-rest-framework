7874bcabe99a321a9f20377d8540c3433f5f79b5
Asif Saifuddin Auvi <auvipy@users.noreply.github.com>
2017-01-04 15:13:32 +0600

    convert some more test asserts to pytest (#4778)
    * converted urlpatterns test asserts to pytest
    
    * converted test utils asserts to pytest
    
    * removed extra newlines

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -65,85 +65,60 @@ class BreadcrumbTests(TestCase):
     """
     def test_root_breadcrumbs(self):
         url = '/'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [('Root', '/')]
-        )
+        assert get_breadcrumbs(url) == [('Root', '/')]
 
     def test_resource_root_breadcrumbs(self):
         url = '/resource/'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource Root', '/resource/')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'), ('Resource Root', '/resource/')
+        ]
 
     def test_resource_instance_breadcrumbs(self):
         url = '/resource/123'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource Root', '/resource/'),
-                ('Resource Instance', '/resource/123')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'),
+            ('Resource Root', '/resource/'),
+            ('Resource Instance', '/resource/123')
+        ]
 
     def test_resource_instance_customname_breadcrumbs(self):
         url = '/resource/customname'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource Root', '/resource/'),
-                ('Foo', '/resource/customname')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'),
+            ('Resource Root', '/resource/'),
+            ('Foo', '/resource/customname')
+        ]
 
     def test_nested_resource_breadcrumbs(self):
         url = '/resource/123/'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource Root', '/resource/'),
-                ('Resource Instance', '/resource/123'),
-                ('Nested Resource Root', '/resource/123/')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'),
+            ('Resource Root', '/resource/'),
+            ('Resource Instance', '/resource/123'),
+            ('Nested Resource Root', '/resource/123/')
+        ]
 
     def test_nested_resource_instance_breadcrumbs(self):
         url = '/resource/123/abc'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource Root', '/resource/'),
-                ('Resource Instance', '/resource/123'),
-                ('Nested Resource Root', '/resource/123/'),
-                ('Nested Resource Instance', '/resource/123/abc')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'),
+            ('Resource Root', '/resource/'),
+            ('Resource Instance', '/resource/123'),
+            ('Nested Resource Root', '/resource/123/'),
+            ('Nested Resource Instance', '/resource/123/abc')
+        ]
 
     def test_broken_url_breadcrumbs_handled_gracefully(self):
         url = '/foobar'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [('Root', '/')]
-        )
+        assert get_breadcrumbs(url) == [('Root', '/')]
 
     def test_modelviewset_resource_instance_breadcrumbs(self):
         url = '/resources/1/'
-        self.assertEqual(
-            get_breadcrumbs(url),
-            [
-                ('Root', '/'),
-                ('Resource List', '/resources/'),
-                ('Resource Instance', '/resources/1/')
-            ]
-        )
+        assert get_breadcrumbs(url) == [
+            ('Root', '/'),
+            ('Resource List', '/resources/'),
+            ('Resource Instance', '/resources/1/')
+        ]
 
 
 class ResolveModelTests(TestCase):
@@ -154,15 +129,15 @@ class ResolveModelTests(TestCase):
     """
     def test_resolve_django_model(self):
         resolved_model = _resolve_model(BasicModel)
-        self.assertEqual(resolved_model, BasicModel)
+        assert resolved_model == BasicModel
 
     def test_resolve_string_representation(self):
         resolved_model = _resolve_model('tests.BasicModel')
-        self.assertEqual(resolved_model, BasicModel)
+        assert resolved_model == BasicModel
 
     def test_resolve_unicode_representation(self):
         resolved_model = _resolve_model(six.text_type('tests.BasicModel'))
-        self.assertEqual(resolved_model, BasicModel)
+        assert resolved_model == BasicModel
 
     def test_resolve_non_django_model(self):
         with self.assertRaises(ValueError):
