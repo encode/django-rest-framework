d2ae41529c2319e1b67f05464b7e543cfb2adc2c
Asif Saifuddin Auvi <auvipy@gmail.com>
2017-01-21 02:25:36 +0600

    fix wrong assert

--- tests/test_relations_slug.py
+++ tests/test_relations_slug.py
@@ -178,7 +178,7 @@ class SlugForeignKeyTests(TestCase):
         data = {'id': 1, 'name': 'source-1', 'target': None}
         instance = ForeignKeySource.objects.get(pk=1)
         serializer = ForeignKeySourceSerializer(instance, data=data)
-        assert serializer.is_valid()
+        assert not serializer.is_valid()
         assert serializer.errors == {'target': ['This field may not be null.']}
 
 
