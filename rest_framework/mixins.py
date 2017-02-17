28e8895b8abcc65457862c828b1118a9d722c8ed
Tomasz Dubiel <bensiu@post.pl>
2017-02-17 14:42:14 -0600

    Correction of parameters

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -19,7 +19,7 @@ class CreateModelMixin(object):
         return request.data
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=self.get_request_data(request))
+        serializer = self.get_serializer(data=self.get_request_data(request, *args, **kwargs))
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
@@ -71,7 +71,7 @@ class UpdateModelMixin(object):
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=self.get_request_data(request), partial=partial)
+        serializer = self.get_serializer(instance, data=self.get_request_data(request, *args, **kwargs), partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
