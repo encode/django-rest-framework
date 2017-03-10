7a8fb262f27a834e8d36558ae57171ec70b51f7d
Tom Christie <tom@tomchristie.com>
2017-03-10 12:07:15 +0000

    Fallback to more widely supported JS feature sets. (#4961)

--- rest_framework/templates/rest_framework/schema.js
+++ rest_framework/templates/rest_framework/schema.js
@@ -1,3 +1,3 @@
-let codec = new window.coreapi.codecs.CoreJSONCodec()
-let coreJSON = window.atob('{{ schema }}')
+var codec = new window.coreapi.codecs.CoreJSONCodec()
+var coreJSON = window.atob('{{ schema }}')
 window.schema = codec.decode(coreJSON)
