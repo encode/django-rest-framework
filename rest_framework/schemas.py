b558c9ecc4d0ebd9d7e3730ea80fa06d9d01591a
Xavier Ordoquy <xordoquy@linovia.com>
2017-03-15 08:07:12 +0100

    Allow the documentation and schema shortcut functions to get a list of urls to introspect.

--- rest_framework/schemas.py
+++ rest_framework/schemas.py
@@ -690,11 +690,13 @@ class SchemaView(APIView):
         return Response(schema)
 
 
-def get_schema_view(title=None, url=None, description=None, urlconf=None, renderer_classes=None, public=False):
+def get_schema_view(
+        title=None, url=None, description=None, urlconf=None,
+        renderer_classes=None, public=False, patterns=None):
     """
     Return a schema view.
     """
-    generator = SchemaGenerator(title=title, url=url, description=description, urlconf=urlconf)
+    generator = SchemaGenerator(title=title, url=url, description=description, urlconf=urlconf, patterns=patterns)
     return SchemaView.as_view(
         renderer_classes=renderer_classes,
         schema_generator=generator,
