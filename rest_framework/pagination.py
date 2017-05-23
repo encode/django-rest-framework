e52d31d09b1b4f2697ec03384be9e57f05c24b6e
Matt Davis <matteius@gmail.com>
2017-05-23 17:34:23 -0400

    Changes to the paginator defaults and settings
    Require a default paginator be specified when using the page size
    setting.
    https://github.com/encode/django-rest-framework/issues/5168

--- rest_framework/pagination.py
+++ rest_framework/pagination.py
@@ -146,6 +146,17 @@ PAGE_BREAK = PageLink(url=None, number=None, is_active=False, is_break=True)
 class BasePagination(object):
     display_page_controls = False
 
+    def __init__(self, *args, **kwargs):
+        # Use of default page size setting requires a default Paginator class
+        if api_settings.PAGE_SIZE and not api_settings.DEFAULT_PAGINATION_CLASS:
+            warnings.warn(
+                "A valid paginator class must be specified with `DEFAULT_PAGINATION_CLASS` "
+                "when using the `PAGE_SIZE` default pagination setting."
+                "Defaulting the setting to specifies `PageNumberPagination` "
+                "is deprecated as pagination is  disabled by default.",
+                DeprecationWarning
+            )
+
     def paginate_queryset(self, queryset, request, view=None):  # pragma: no cover
         raise NotImplementedError('paginate_queryset() must be implemented.')
 
