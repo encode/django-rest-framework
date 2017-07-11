c8b4a8ddd51fa10933ff8cf98f541fbcf7778a49
Ryan P Kilby <rpkilby@ncsu.edu>
2017-07-11 11:53:20 -0400

    Add docstring to json wrapper module

--- rest_framework/utils/json.py
+++ rest_framework/utils/json.py
@@ -1,3 +1,10 @@
+"""
+Wrapper for the builtin json module that ensures compliance with the JSON spec.
+
+REST framework should always import this wrapper module in order to maintain
+spec-compliant encoding/decoding. Support for non-standard features should be
+handled by users at the renderer and parser layer.
+"""
 
 from __future__ import absolute_import
 
