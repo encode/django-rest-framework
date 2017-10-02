5d10c2eb647890430dd7ddac80efce4f1e50bb04
Carlton Gibson <carlton.gibson@noumenal.es>
2017-10-02 10:48:05 +0200

    Add failing test for #5395

--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,11 @@
 """
-Blank URLConf just to keep the test suite happy
+URLConf for test suite.
+
+We need only the docs urls for DocumentationRenderer tests.
 """
-urlpatterns = []
+from django.conf.urls import url
+from rest_framework.documentation import include_docs_urls
+
+urlpatterns = [
+    url(r'^docs/', include_docs_urls(title='Test Suite API')),
+]
