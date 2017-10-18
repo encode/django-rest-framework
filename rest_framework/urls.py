1a526c153e2aea541bdea054f589b720901ddee1
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-10-18 09:46:27 +0200

    Fixed Django 2.1 compatibility due to removal of django.contrib.auth.login()/logout() views. (#5510)

--- rest_framework/urls.py
+++ rest_framework/urls.py
@@ -15,12 +15,11 @@ and you should make sure your authentication settings include `SessionAuthentica
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.contrib.auth import views
 
-template_name = {'template_name': 'rest_framework/login.html'}
+from rest_framework.compat import login, login_kwargs, logout
 
 app_name = 'rest_framework'
 urlpatterns = [
-    url(r'^login/$', views.login, template_name, name='login'),
-    url(r'^logout/$', views.logout, template_name, name='logout'),
+    url(r'^login/$', login, login_kwargs, name='login'),
+    url(r'^logout/$', logout, name='logout'),
 ]
