64cfa3b64ddfc51d588ffcf4b946dd7514b7365b
Jon Dufresne <jon.dufresne@gmail.com>
2017-11-03 16:03:05 -0700

    Add trove classifier for Python 3.6 support.
    Helps users know, at a glance, if the library is compatible with a
    project.

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
