652e30d7febac75f0cf989f22b80076de75095f2
Ryan P Kilby <rpkilby@ncsu.edu>
2017-12-21 13:10:55 -0500

    Remove 'MIDDLEWARE_CLASSES' compat setting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,6 @@
 def pytest_configure():
     from django.conf import settings
 
-    MIDDLEWARE = (
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-    )
-
     settings.configure(
         DEBUG_PROPAGATE_EXCEPTIONS=True,
         DATABASES={
@@ -31,8 +24,12 @@ def pytest_configure():
                 }
             },
         ],
-        MIDDLEWARE=MIDDLEWARE,
-        MIDDLEWARE_CLASSES=MIDDLEWARE,
+        MIDDLEWARE=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ),
         INSTALLED_APPS=(
             'django.contrib.auth',
             'django.contrib.contenttypes',
