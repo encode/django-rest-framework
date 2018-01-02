6b0bf72bb8ba3e6ff1e7a334e6a23d95e8c2944a
David De Sousa <davidesousa@gmail.com>
2018-01-02 10:59:08 +0100

    using rsplit to get module and classname for imports (#5712)

--- rest_framework/settings.py
+++ rest_framework/settings.py
@@ -179,8 +179,7 @@ def import_from_string(val, setting_name):
     """
     try:
         # Nod to tastypie's use of importlib.
-        parts = val.split('.')
-        module_path, class_name = '.'.join(parts[:-1]), parts[-1]
+        module_path, class_name = val.rsplit('.', 1)
         module = import_module(module_path)
         return getattr(module, class_name)
     except (ImportError, AttributeError) as e:
