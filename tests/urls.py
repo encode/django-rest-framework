1a170438d2069939f55bef0647bed4b2f8ffc44e
Ryan P Kilby <rpkilby@ncsu.edu>
2018-06-22 04:16:57 -0400

    Add "optionals not required" build (#6047)

--- tests/urls.py
+++ tests/urls.py
@@ -5,8 +5,12 @@ We need only the docs urls for DocumentationRenderer tests.
 """
 from django.conf.urls import url
 
+from rest_framework.compat import coreapi
 from rest_framework.documentation import include_docs_urls
 
-urlpatterns = [
-    url(r'^docs/', include_docs_urls(title='Test Suite API')),
-]
+if coreapi:
+    urlpatterns = [
+        url(r'^docs/', include_docs_urls(title='Test Suite API')),
+    ]
+else:
+    urlpatterns = []
