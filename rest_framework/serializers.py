84c695d813b00b1fd0c177ac38d18fe7c471a22a
Oleg Nykolyn <juravel2@gmail.com>
2018-08-06 20:39:49 +0300

    Improve ModelSerializer.create() error message.

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -941,15 +941,17 @@ class ModelSerializer(Serializer):
         except TypeError:
             tb = traceback.format_exc()
             msg = (
-                'Got a `TypeError` when calling `%s._default_manager.create()`. '
+                'Got a `TypeError` when calling `%s.%s.create()`. '
                 'This may be because you have a writable field on the '
                 'serializer class that is not a valid argument to '
-                '`%s._default_manager.create()`. You may need to make the field '
+                '`%s.%s.create()`. You may need to make the field '
                 'read-only, or override the %s.create() method to handle '
                 'this correctly.\nOriginal exception was:\n %s' %
                 (
                     ModelClass.__name__,
+                    ModelClass._default_manager.name,
                     ModelClass.__name__,
+                    ModelClass._default_manager.name,
                     self.__class__.__name__,
                     tb
                 )
