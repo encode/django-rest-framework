7428f58ee423a56325622d8ca4e0ad4a10b2e5af
Carlton Gibson <carlton.gibson@noumenal.es>
2018-08-09 10:03:49 +0200

    Add DEFAULT_SCHEMA_GENERATOR_CLASS setting

--- rest_framework/management/commands/generate_schema.py
+++ rest_framework/management/commands/generate_schema.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from rest_framework.renderers import CoreJSONRenderer
-from rest_framework.schemas import SchemaGenerator
+from rest_framework.renderers import CoreJSONRenderer, JSONRenderer
+from rest_framework.schemas.generators import OpenAPISchemaGenerator
+from rest_framework.settings import api_settings
 
 
 class Command(BaseCommand):
@@ -26,11 +27,21 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        generator_class = self._get_generator_class()
+        generator = generator_class()
 
-        renderer = CoreJSONRenderer()
-        generator = SchemaGenerator()
         schema = generator.get_schema(request=None, public=True)
 
+        renderer = self._get_renderer(generator)
         rendered_schema = renderer.render(schema, renderer_context={}).decode('utf8')
 
         self.stdout.write(rendered_schema)
+
+    def _get_generator_class(self):
+        return api_settings.DEFAULT_SCHEMA_GENERATOR_CLASS
+
+    def _get_renderer(self, generator):
+        if isinstance(generator, OpenAPISchemaGenerator):
+            return JSONRenderer()
+        else:
+            return CoreJSONRenderer()
