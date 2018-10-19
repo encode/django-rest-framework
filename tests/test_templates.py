9c2389901730c2ef01b8544eb80c3588383b0fb0
jeffrey k eliasen <jeff@jke.net>
2018-10-18 17:05:12 -0700

    template includes csrfToken property in javascript

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,7 +1,11 @@
+import re
+
 from django.shortcuts import render
 
 
 def test_base_template_with_no_context():
     # base.html should be renderable with no context,
     # so it can be easily extended.
-    render({}, 'rest_framework/base.html')
+    result = render({}, 'rest_framework/base.html')
+    # note that this response will not include a valid CSRF token
+    assert re.search(r'\bcsrfToken: ""', result.content)
