e7c6cc435792d40b9bfe2d2bae3147cfc5dd9395
jeffrey k eliasen <jeff@jke.net>
2018-10-22 19:43:15 -1000

    fix broken tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -8,4 +8,4 @@ def test_base_template_with_no_context():
     # so it can be easily extended.
     result = render({}, 'rest_framework/base.html')
     # note that this response will not include a valid CSRF token
-    assert re.search(r'\bcsrfToken: ""', result.content)
+    assert re.search(r'\bcsrfToken: ""', result.content.decode('utf-8'))
