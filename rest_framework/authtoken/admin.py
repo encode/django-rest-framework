b17eaba8e125a75b4e1e358b167442723fb4987d
Ben Wilber <benwilber@gmail.com>
2019-02-04 14:05:03 -0500

    Use raw user IDs in authtoken admin

--- rest_framework/authtoken/admin.py
+++ rest_framework/authtoken/admin.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 class TokenAdmin(admin.ModelAdmin):
     list_display = ('key', 'user', 'created')
     fields = ('user',)
+    raw_id_fields = ('user',)
     ordering = ('-created',)
 
 
