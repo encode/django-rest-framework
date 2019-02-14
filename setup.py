65f5c11a5b8aca5a6aca14c7ad85293fa941c37d
Carlton Gibson <carlton.gibson@noumenal.es>
2019-02-14 15:38:21 +0100

    Document support for Django 2.2.

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
