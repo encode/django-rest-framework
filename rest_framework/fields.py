9d06e43d05abf1ec57f15566b29ad53ac418ae05
Jon Dufresne <jon.dufresne@gmail.com>
2019-03-05 03:11:46 -0800

    Replace type('') with six.text_type (#6482)
    As all source files import unicode_literals, type('') is always
    equivalent to six.text_type (str on Python 3 and unicode on Python 2).
    Removes the need to call type(), is more explicit, and will be easier to
    catch places to change for when it is time to eventually drop Python 2.

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -1486,7 +1486,7 @@ class MultipleChoiceField(ChoiceField):
         return dictionary.get(self.field_name, empty)
 
     def to_internal_value(self, data):
-        if isinstance(data, type('')) or not hasattr(data, '__iter__'):
+        if isinstance(data, six.text_type) or not hasattr(data, '__iter__'):
             self.fail('not_a_list', input_type=type(data).__name__)
         if not self.allow_empty and len(data) == 0:
             self.fail('empty')
@@ -1660,7 +1660,7 @@ class ListField(Field):
         """
         if html.is_html_input(data):
             data = html.parse_html_list(data, default=[])
-        if isinstance(data, (type(''), Mapping)) or not hasattr(data, '__iter__'):
+        if isinstance(data, (six.text_type, Mapping)) or not hasattr(data, '__iter__'):
             self.fail('not_a_list', input_type=type(data).__name__)
         if not self.allow_empty and len(data) == 0:
             self.fail('empty')
