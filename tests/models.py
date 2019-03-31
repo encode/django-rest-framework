0425a943fa15a8d4fc9aeb02738ee105db5cdcca
Asif Saif Uddin <auvipy@gmail.com>
2019-03-31 09:40:19 +0600

    fix deprecation warning

--- tests/models.py
+++ tests/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import uuid
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class RESTFrameworkModel(models.Model):
