2d689339681af462409642935e1c0a7430faf846
Asif Saif Uddin <auvipy@gmail.com>
2019-03-31 09:35:19 +0600

    fix deprecation warning

--- rest_framework/authtoken/models.py
+++ rest_framework/authtoken/models.py
@@ -4,7 +4,7 @@ import os
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 @python_2_unicode_compatible
