1bc299f87fd4b4af2694bb6d3381ae6b053d0835
Tim Gates <tim.gates@iress.com>
2019-04-11 15:10:56 +1000

    Adjust indenting for flake8 linting errors

--- rest_framework/compat.py
+++ rest_framework/compat.py
@@ -188,8 +188,8 @@ try:
         HEADERID_EXT_PATH = 'headerid'
         LEVEL_PARAM = 'level'
     elif (not hasattr(markdown, '__version__') or
-            (isinstance(markdown.__version__, type(''))
-            and markdown.version < '2.6')):
+          (isinstance(markdown.__version__, type(''))
+           and markdown.version < '2.6')):
         HEADERID_EXT_PATH = 'markdown.extensions.headerid'
         LEVEL_PARAM = 'level'
     else:
