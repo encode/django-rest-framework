83d09c7bc53c6c0b79e400fe05e1aff6788c2b2d
Carlton Gibson <carlton.gibson@noumenal.es>
2019-04-29 16:30:44 +0200

    Update version and release notes for v3.9.3.

--- rest_framework/__init__.py
+++ rest_framework/__init__.py
@@ -8,7 +8,7 @@ ______ _____ _____ _____    __
 """
 
 __title__ = 'Django REST framework'
-__version__ = '3.9.2'
+__version__ = '3.9.3'
 __author__ = 'Tom Christie'
 __license__ = 'BSD 2-Clause'
 __copyright__ = 'Copyright 2011-2019 Encode OSS Ltd'
