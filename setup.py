ffe7a79ad009b3f29e035aaa30f2b77a0ba78f36
Asif Saif Uddin <auvipy@gmail.com>
2019-04-30 14:56:32 +0600

    Dropped Python 3.4 support

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     install_requires=[],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*,",
     zip_safe=False,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -69,7 +69,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
