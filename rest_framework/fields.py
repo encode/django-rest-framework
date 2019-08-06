7e1c4be7ced1fe242540c796d046d665f2a40350
zeroohub <spamzero@yeah.net>
2019-08-06 18:31:46 +0800

    Avoided double call for remote url in FileField.to_representation. (#6841)

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -1546,16 +1546,16 @@ class FileField(Field):
             return None
 
         use_url = getattr(self, 'use_url', api_settings.UPLOADED_FILES_USE_URL)
-
         if use_url:
-            if not getattr(value, 'url', None):
-                # If the file has not been saved it may not have a URL.
+            try:
+                url = value.url
+            except AttributeError:
                 return None
-            url = value.url
             request = self.context.get('request', None)
             if request is not None:
                 return request.build_absolute_uri(url)
             return url
+
         return value.name
 
 
