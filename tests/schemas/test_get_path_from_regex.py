80394386d07dd40474a4e2fc772e7d00c35f0e20
Alan Crosswell <alan@columbia.edu>
2019-08-23 14:53:53 -0400

    demonstrate success with trailing /

--- tests/schemas/test_get_path_from_regex.py
+++ tests/schemas/test_get_path_from_regex.py
@@ -5,7 +5,12 @@ def test_get_path_from_regex():
     Test that path from regex is correctly formatted.
     """
     path_regex = r'^entries/(?P<pk>[^/.]+)/relationships/(?P<related_field>\w+)'
+    path_regex_trailing = path_regex + '/'
     expected_path = '/entries/{pk}/relationships/{related_field}'
+    expected_path_trailing = expected_path + '/'
     enumerator = EndpointEnumerator(patterns={})
+    path_trailing = enumerator.get_path_from_regex(path_regex_trailing)
+    assert path_trailing == expected_path_trailing
+
     path = enumerator.get_path_from_regex(path_regex)
     assert path == expected_path
