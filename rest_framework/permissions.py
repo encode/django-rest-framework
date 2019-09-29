4a24e63af69903d7cc387c28788373b083b0a8ef
صهيب السلامات <36170722+sohepalslamat@users.noreply.github.com>
2019-09-29 16:34:48 +0300

    Update permissions.py
    Allows access only to super users.

--- rest_framework/permissions.py
+++ rest_framework/permissions.py
@@ -145,6 +145,14 @@ class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_staff)
 
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to super users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
 
 class IsAuthenticatedOrReadOnly(BasePermission):
     """
