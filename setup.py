de855141b7c20c3ae1cfea4a24b1e6039ce21f9d
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2019-10-24 10:16:43 +0600

    django 3.0 in matrix with allow failure

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
