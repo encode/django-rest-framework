9c934edc786e0bddec658b9c2fe3d45ad1b4f731
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2019-10-29 02:34:59 +0300

    use @admin decorator

--- rest_framework/authtoken/admin.py
+++ rest_framework/authtoken/admin.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 from rest_framework.authtoken.models import Token
 
 
+@admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
     list_display = ('key', 'user', 'created')
     fields = ('user',)
     ordering = ('-created',)
-
-
-admin.site.register(Token, TokenAdmin)
