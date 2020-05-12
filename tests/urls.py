4c90760aefc4fe28df15b591f4996e94a2aa4345
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2020-05-12 10:18:49 +0600

    url() -> path()

--- tests/urls.py
+++ tests/urls.py
@@ -3,14 +3,14 @@ URLConf for test suite.
 
 We need only the docs urls for DocumentationRenderer tests.
 """
-from django.conf.urls import url
+from django.urls import path
 
 from rest_framework.compat import coreapi
 from rest_framework.documentation import include_docs_urls
 
 if coreapi:
     urlpatterns = [
-        url(r'^docs/', include_docs_urls(title='Test Suite API')),
+        path('docs/', include_docs_urls(title='Test Suite API')),
     ]
 else:
     urlpatterns = []
