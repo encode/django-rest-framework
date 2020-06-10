7f2b8815b2c923b68d5951fa7467038e4177e45d
Pritesh K <pkanani@logilite.com>
2020-06-10 13:55:10 +0530

    Added IsSuperUser permission

--- rest_framework/permissions.py
+++ rest_framework/permissions.py
@@ -137,6 +137,15 @@ class IsAuthenticated(BasePermission):
         return bool(request.user and request.user.is_authenticated)
 
 
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to super users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
+
 class IsAdminUser(BasePermission):
     """
     Allows access only to admin users.
