db70ef97c84f37d214f7efd632d9156971c4fffb
Aristotelis Mikropoulos <amikrop@gmail.com>
2020-10-15 22:46:18 +0300

    Fix indentation in test

--- tests/test_relations.py
+++ tests/test_relations.py
@@ -107,7 +107,7 @@ class TestPrimaryKeyRelatedField(APISimpleTestCase):
         msg = excinfo.value.detail[0]
         assert msg == 'Incorrect type. Expected pk value, received BadType.'
 
-     def test_pk_related_lookup_bool(self):
+    def test_pk_related_lookup_bool(self):
         with pytest.raises(serializers.ValidationError) as excinfo:
             self.field.to_internal_value(True)
         msg = excinfo.value.detail[0]
