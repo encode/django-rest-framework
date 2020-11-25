f7f71f37c9bb8f3340bf0b5e80809a250ab20af0
sarath <aksarath@gmail.com>
2020-11-25 07:30:45 +0530

    Allow 'get_page' method for overriding #7626

--- rest_framework/pagination.py
+++ rest_framework/pagination.py
@@ -198,7 +198,7 @@ class PageNumberPagination(BasePagination):
             return None
 
         paginator = self.django_paginator_class(queryset, page_size)
-        page_number = request.query_params.get(self.page_query_param, 1)
+        page_number = self.get_page_number(request)
         if page_number in self.last_page_strings:
             page_number = paginator.num_pages
 
@@ -216,6 +216,9 @@ class PageNumberPagination(BasePagination):
 
         self.request = request
         return list(self.page)
+
+    def get_page_number(self, request):
+        return request.query_params.get(self.page_query_param, 1)
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
