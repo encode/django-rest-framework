55f328e2155a7726d5c394c087a8cf84cca632f1
Dawid Kosiński <tofik1432@gmail.com>
2021-02-21 17:32:12 +0100

    Resolves #5127

--- tests/browsable_api/no_auth_urls.py
+++ tests/browsable_api/no_auth_urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import BasicModelWithUsersViewSet, MockView
 
-
 urlpatterns = [
     path('', MockView.as_view()),
     path('basicviewset', BasicModelWithUsersViewSet.as_view({'get': 'list'})),
