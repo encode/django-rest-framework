98ad5f38eb269e1af4883d0770eaac9ed2a119a5
Tom Christie <tom@tomchristie.com>
2021-03-16 13:27:06 +0000

    Revert "made Browsable API base template cachable: omit CSRF token when unnecessary (#7717)"
    This reverts commit 9c9ffb18f44062fd05f0b4e06b756c0a35230561.

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -3,23 +3,15 @@ import re
 from django.shortcuts import render
 
 
+def test_base_template_with_context():
+    context = {'request': True, 'csrf_token': 'TOKEN'}
+    result = render({}, 'rest_framework/base.html', context=context)
+    assert re.search(r'\bcsrfToken: "TOKEN"', result.content.decode())
+
+
 def test_base_template_with_no_context():
     # base.html should be renderable with no context,
     # so it can be easily extended.
     result = render({}, 'rest_framework/base.html')
     # note that this response will not include a valid CSRF token
     assert re.search(r'\bcsrfToken: ""', result.content.decode())
-
-
-def test_base_template_with_simple_context():
-    context = {'request': True, 'csrf_token': 'TOKEN'}
-    result = render({}, 'rest_framework/base.html', context=context)
-    # note that response will STILL not include a CSRF token
-    assert re.search(r'\bcsrfToken: ""', result.content.decode())
-
-
-def test_base_template_with_editing_context():
-    context = {'request': True, 'post_form': object(), 'csrf_token': 'TOKEN'}
-    result = render({}, 'rest_framework/base.html', context=context)
-    # response includes a CSRF token in support of the POST form
-    assert re.search(r'\bcsrfToken: "TOKEN"', result.content.decode())
