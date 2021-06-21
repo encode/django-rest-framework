4fdc1caeaf8a8100775e62974cc24b6b895976b1
Adam Johnson <me@adamj.eu>
2021-06-21 11:26:52 +0100

    Add missed PyPI classifier

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
