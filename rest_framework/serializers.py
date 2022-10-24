61becb6bc634720ca7669c64079886f6b106738a
954 <510485871@qq.com>
2022-10-25 07:58:26 +0800

    Change to only BaseManager

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -681,9 +681,7 @@ class ListSerializer(BaseSerializer):
         """
         # Dealing with nested relationships, data can be a Manager,
         # so, first get a queryset from the Manager if needed
-        iterable = data.all() if isinstance(data, (
-            models.manager.Manager, models.manager.BaseManager
-        )) else data
+        iterable = data.all() if isinstance(data, models.manager.BaseManager) else data
 
         return [
             self.child.to_representation(item) for item in iterable
