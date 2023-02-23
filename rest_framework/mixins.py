c2921b1eca4682fc7e10dbdf34065d2611d4fc4a
Adam Dobrawy <adam@getselectstar.com>
2023-02-23 04:28:00 +0100

    Reduce diff

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -36,6 +36,7 @@ class ListModelMixin:
     """
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
+
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
