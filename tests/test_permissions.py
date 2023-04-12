02011083944757c80ea942153c60c1d5c7f70f83
Asif Saif Uddin <auvipy@gmail.com>
2023-04-12 22:09:17 +0600

    Update tests/test_permissions.py

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -530,7 +530,7 @@ class DeniedObjectViewWithDetailAND3(PermissionInstanceView):
 
 
 class DeniedObjectViewWithDetailOR1(PermissionInstanceView):
-    permission_classes = (BasicObjectPerm | BasicObjectPermWithDetail,)
+    permission_classes = (BasicObjectPerm | BasicObjectPermWithDetail)
 
 
 class DeniedObjectViewWithDetailOR2(PermissionInstanceView):
