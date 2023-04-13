721df598cc6a03e8c9d710547a39d2527c9ab506
Asif Saif Uddin <auvipy@gmail.com>
2023-04-13 21:50:38 +0600

    Update rest_framework/metadata.py

--- rest_framework/metadata.py
+++ rest_framework/metadata.py
@@ -125,7 +125,7 @@ class SimpleMetadata(BaseMetadata):
             'read_only', 'label', 'help_text',
             'min_length', 'max_length',
             'min_value', 'max_value',
-            'max_digits', 'decimal_places',
+            'max_digits', 'decimal_places'
         ]
 
         for attr in attrs:
