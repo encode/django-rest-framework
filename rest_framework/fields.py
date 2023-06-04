376a5cbbba3f8df9c9db8c03a7c8fa2a6e6c05f4
Mathieu Dupuy <mathieu.dupuy@gitguardian.com>
2023-06-04 07:24:07 +0200

    remove dependency on pytz (#8984)
    * remove pytz
    
    * Revert "remove pytz"
    
    This reverts commit 393609dfaadbc4d82f760e6e4df8a6de6de24f9b.
    
    * remove pytz, more subtly

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -29,7 +29,11 @@ from django.utils.encoding import is_protected_type, smart_str
 from django.utils.formats import localize_input, sanitize_separators
 from django.utils.ipv6 import clean_ipv6_address
 from django.utils.translation import gettext_lazy as _
-from pytz.exceptions import InvalidTimeError
+
+try:
+    import pytz
+except ImportError:
+    pytz = None
 
 from rest_framework import ISO_8601
 from rest_framework.exceptions import ErrorDetail, ValidationError
@@ -1148,8 +1152,10 @@ class DateTimeField(Field):
                 if not valid_datetime(dt):
                     self.fail('make_aware', timezone=field_timezone)
                 return dt
-            except InvalidTimeError:
-                self.fail('make_aware', timezone=field_timezone)
+            except Exception as e:
+                if pytz and isinstance(e, pytz.exceptions.InvalidTimeError):
+                    self.fail('make_aware', timezone=field_timezone)
+                raise e
         elif (field_timezone is None) and timezone.is_aware(value):
             return timezone.make_naive(value, datetime.timezone.utc)
         return value
