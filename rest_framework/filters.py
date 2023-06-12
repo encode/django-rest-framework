43eb05e258f9a9d43f44a31a7c2a1f4d734cd67e
Asif Saif Uddin <auvipy@gmail.com>
2023-06-12 12:39:41 +0600

    Update rest_framework/filters.py

--- rest_framework/filters.py
+++ rest_framework/filters.py
@@ -151,7 +151,7 @@ class SearchFilter(BaseFilterBackend):
     def get_schema_fields(self, view):
         assert coreapi is not None, 'coreapi must be installed to use `get_schema_fields()`'
         if coreapi is not None:
-            warnings.warn('CoreAPI compatibility is deprecated and will be removed in DRF 3.15', RemovedInDRF315Warning)
+            warnings.warn('CoreAPI compatibility is deprecated and will be removed in DRF 3.17', RemovedInDRF317Warning)
         assert coreschema is not None, 'coreschema must be installed to use `get_schema_fields()`'
         return [
             coreapi.Field(
