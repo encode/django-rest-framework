aed7761a8d7e1691a4f4bbf9c83a447dac44d92a
Asif Saif Uddin <auvipy@gmail.com>
2023-06-13 15:01:29 +0600

    Update copy right timeline

--- rest_framework/__init__.py
+++ rest_framework/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'Django REST framework'
 __version__ = '3.14.0'
 __author__ = 'Tom Christie'
 __license__ = 'BSD 3-Clause'
-__copyright__ = 'Copyright 2011-2019 Encode OSS Ltd'
+__copyright__ = 'Copyright 2011-2023 Encode OSS Ltd'
 
 # Version synonym
 VERSION = __version__
