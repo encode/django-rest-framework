bee53973cdcd9f46359b52865caea90afaaa2ee2
Andrii Bronetskyi <38384812+abronets@users.noreply.github.com>
2023-08-24 14:55:26 +0300

    Added request param to paginate_queryset in ListModelMixin

--- rest_framework/mixins.py
+++ rest_framework/mixins.py
@@ -39,7 +39,7 @@ class ListModelMixin:
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        page = self.paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset, request)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
