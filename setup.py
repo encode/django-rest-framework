37ec04d518f8af0e48bb319b5b3555492170aec4
Rodrigo <matematica.a3k@gmail.com>
2024-02-20 10:16:03 -0300

    Add Python 3.12 support (#9230)
    * added python 3.12 to CI
    
    * Update setup.py
    
    ---------
    
    Co-authored-by: Asif Saif Uddin <auvipy@gmail.com>

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
     ],
