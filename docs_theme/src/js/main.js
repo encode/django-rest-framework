3504aa148538196ff45e16dc85c77c48344fa75c
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-05-28 18:27:22 +0330

    fix: optimize js and remove unnecessary imports

--- docs_theme/src/js/main.js
+++ docs_theme/src/js/main.js
@@ -1,15 +1,15 @@
-import "../../node_modules/bootstrap/js/dist/alert";
+// import "../../node_modules/bootstrap/js/dist/alert";
 import "../../node_modules/bootstrap/js/dist/button";
-import "../../node_modules/bootstrap/js/dist/carousel";
+// import "../../node_modules/bootstrap/js/dist/carousel";
 import "../../node_modules/bootstrap/js/dist/collapse";
 import "../../node_modules/bootstrap/js/dist/dropdown";
 import "../../node_modules/bootstrap/js/dist//modal";
-import "../../node_modules/bootstrap/js/dist/offcanvas";
-import "../../node_modules/bootstrap/js/dist/popover";
-import "../../node_modules/bootstrap/js/dist/scrollspy";
-import "../../node_modules/bootstrap/js/dist/tab";
-import "../../node_modules/bootstrap/js/dist/toast";
-import "../../node_modules/bootstrap/js/dist/tooltip";
+// import "../../node_modules/bootstrap/js/dist/offcanvas";
+// import "../../node_modules/bootstrap/js/dist/popover";
+// import "../../node_modules/bootstrap/js/dist/scrollspy";
+// import "../../node_modules/bootstrap/js/dist/tab";
+// import "../../node_modules/bootstrap/js/dist/toast";
+// import "../../node_modules/bootstrap/js/dist/tooltip";
 
 import "../scss/styles.scss";
 
