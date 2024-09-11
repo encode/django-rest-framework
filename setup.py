b25028ac8f46984459f9d9e28069b797986ae476
Bruno Alla <browniebroke@users.noreply.github.com>
2024-09-11 10:39:52 +0100

    Add support for Python 3.13 (#9527)
    * Add support for Python 3.13
    
    * Fix extracting tox env with -dev Python versions
    
    * Fix view description inspection in Python 3.13
    
    Python 3.13 introduced docstrings for None: https://github.com/python/cpython/pull/117813
    
    In Python 3.12, this is an empty string:
    
    ```
     ➜ python3.12
    Python 3.12.6 (main, Sep 10 2024, 19:06:17) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
    Type "help", "copyright", "credits" or "license" for more information.
    >>> d = None
    >>> d.__doc__
    >>>
    ```
    
    In Python 3.13, it's no longer empty:
    
    ```
     ➜ python3.13
    Python 3.13.0rc2+ (heads/3.13:660baa1, Sep 10 2024, 18:57:50) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
    Type "help", "copyright", "credits" or "license" for more information.
    >>> d = None
    >>> d.__doc__
    'The type of the None singleton.'
    >>>
    ```
    
    Adding a check in the inspector that get the view description out the view function docstring to catch this edge case.

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
     ],
