ad3b986530106e86c9d6bd6d226496b9df4bd263
Bruno Alla <alla.brunoo@gmail.com>
2025-08-05 23:37:12 +0100

    Bump package version

--- rest_framework/__init__.py
+++ rest_framework/__init__.py
@@ -8,7 +8,7 @@ ______ _____ _____ _____    __
 """
 
 __title__ = 'Django REST framework'
-__version__ = '3.16.0'
+__version__ = '3.16.1'
 __author__ = 'Tom Christie'
 __license__ = 'BSD 3-Clause'
 __copyright__ = 'Copyright 2011-2023 Encode OSS Ltd'
