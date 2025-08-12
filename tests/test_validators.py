053eb1e213c7369cab4de683d7128a90b7a10089
Nicolas Delaby <nicolas@dryad.net>
2025-08-12 17:08:28 +0200

    Condition of UniqueValidator can be read-only
    We can't always expect to find the value of the condition in the serializer
    if the field is read-only.

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -589,6 +589,21 @@ class UniqueConstraintModel(models.Model):
         ]
 
 
+class UniqueConstraintReadOnlyFieldModel(models.Model):
+    state = models.CharField(max_length=100, default="new")
+    position = models.IntegerField()
+    something = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_constraint_%(class)s",
+                fields=("position", "something"),
+                condition=models.Q(state="new"),
+            ),
+        ]
+
+
 class UniqueConstraintNullableModel(models.Model):
     title = models.CharField(max_length=100)
     age = models.IntegerField(null=True)
@@ -737,6 +752,23 @@ class TestUniqueConstraintValidation(TestCase):
             }
         )
         assert serializer.is_valid()
+
+    def test_uniq_constraint_condition_read_only(self):
+        class UniqueConstraintReadOnlyFieldModelSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = UniqueConstraintReadOnlyFieldModel
+                read_only_fields = ("state",)
+                fields = ("position", "something", *read_only_fields)
+
+        serializer = UniqueConstraintReadOnlyFieldModelSerializer(
+            data={"position": 1, "something": 1}
+        )
+        assert serializer.is_valid()
+        UniqueConstraintReadOnlyFieldModel.objects.create(position=1, something=1)
+        serializer = UniqueConstraintReadOnlyFieldModelSerializer(
+            data={"position": 1, "something": 1}
+        )
+        assert not serializer.is_valid()
 
 
 # Tests for `UniqueForDateValidator`
