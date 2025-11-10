7166a78520c527ef107a85d91b39823ed7643e49
shrajal01 <shrajal.2428it1420@kiet.edu>
2025-11-10 22:15:58 +0530

    Refactor get_initial for list input handlingFix ListSerializer.get_initial to return consistent initial list structure
    Modify get_initial to return a consistent list format when initial_data is a list.

--- rest_framework/serializers.py
+++ rest_framework/serializers.py
@@ -608,10 +608,15 @@ class ListSerializer(BaseSerializer):
         super().__init__(*args, **kwargs)
         self.child.bind(field_name='', parent=self)
 
-    def get_initial(self):
-        if hasattr(self, 'initial_data'):
-            return self.to_representation(self.initial_data)
+   def get_initial(self):
+    if hasattr(self, 'initial_data'):
+        # If data is given, we should just return the raw input structure,
+        # but ensure it's represented in a consistent list format.
+        if isinstance(self.initial_data, list):
+            return [self.child.get_initial() for _ in self.initial_data]
         return []
+    return []
+
 
     def get_value(self, dictionary):
         """
